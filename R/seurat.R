9f47145dad99640eadba2d8f8515c355b6ef3b60
timoast <timstuart90@gmail.com>
2017-03-30 10:03:06 +0800

    Check if DE genes is null

--- R/seurat.R
+++ R/seurat.R
@@ -2083,6 +2083,7 @@ setMethod("FindAllMarkers","seurat",
             gde.all=data.frame()
             for(i in 1:length(idents.all)) {
               gde=genes.de[[i]]
+              if (is.null(gde)) next;
               if (nrow(gde)>0) {
                 if (test.use=="roc") {
                   gde=subset(gde,(myAUC>return.thresh|myAUC<(1-return.thresh)))
