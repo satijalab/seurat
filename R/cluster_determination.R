c50b24f4b78e9cbd4846a74a8f7a293861ea5790
Paul Hoffman <hoff0792@umn.edu>
2017-06-16 14:04:25 -0400

    Remove generics and method no longer needed; refactor code

--- R/cluster_determination.R
+++ R/cluster_determination.R
@@ -5,9 +5,8 @@ NULL
 #' Identify clusters of cells by a shared nearest neighbor (SNN) modularity optimization
 #' based clustering algorithm. First calculate k-nearest neighbors and construct
 #' the SNN graph. Then optimize the modularity function to determine clusters.
-#' For a full description of the algorithms, see Waltman and van Eck (2013) 
+#' For a full description of the algorithms, see Waltman and van Eck (2013)
 #' \emph{The European Physical Journal B}.
-#'
 #'
 #' @param object Seurat object
 #' @param genes.use Gene expression data
@@ -33,244 +32,313 @@ NULL
 #' @param n.iter Maximal number of iterations per random start.
 #' @param random.seed Seed of the random number generator.
 #' @param print.output Whether or not to print output to the console
-#' @param temp.file.location Directory where intermediate files will be written. Specify the 
+#' @param temp.file.location Directory where intermediate files will be written. Specify the
 #' @param distance.matrix Build SNN from distance matrix (experimental)
 #' ABSOLUTE path.
+#'
 #' @importFrom FNN get.knn
 #' @importFrom igraph plot.igraph graph.adjlist
 #' @importFrom Matrix sparseMatrix
+#'
 #' @return Returns a Seurat object and optionally the SNN matrix,
 #'         object@@ident has been updated with new cluster info
+#'
 #' @export
-setGeneric("FindClusters", function(object, genes.use = NULL, reduction.type="pca",pc.use = NULL,
-                                    k.param = 30, k.scale = 25,
-                                    plot.SNN = FALSE, prune.SNN = 1/15,
-                                    save.SNN = FALSE, reuse.SNN = FALSE,
-                                    do.sparse = FALSE, modularity.fxn = 1, 
-                                    resolution = 0.8, algorithm = 1, 
-                                    n.start = 100, n.iter = 10, random.seed = 0,
-                                    print.output = TRUE, temp.file.location = NULL, distance.matrix=NULL)
-  standardGeneric("FindClusters"))
-#' @export
-setMethod("FindClusters", signature = "seurat",
-          function(object, genes.use = NULL, reduction.type="pca",pc.use = NULL, k.param = 30,
-                   k.scale = 25, plot.SNN = FALSE, prune.SNN = 1/15,
-                   save.SNN = FALSE, reuse.SNN = FALSE, do.sparse = FALSE, 
-                   modularity.fxn = 1, resolution = 0.8, algorithm = 1,
-                   n.start = 100, n.iter = 10, random.seed = 0, print.output = TRUE,
-                   temp.file.location = NULL,distance.matrix=NULL){
-            
-          # for older objects without the snn.k slot
-          if(typeof(validObject(object, test = T)) == "character"){
-            object@snn.k <- numeric()
-          }
-          
-          snn.built <- FALSE
-          if (.hasSlot(object, "snn.dense")) {
-            if (length(object@snn.dense) > 1) {
-              snn.built <- TRUE
-            }
-          }
-          if (.hasSlot(object, "snn.sparse")) {
-            if (length(object@snn.sparse) > 1) {
-              snn.built <- TRUE
-            }
-          }
-          
-          if((missing(genes.use) && missing(pc.use) && missing(k.param) && missing(k.scale) && 
-              missing(prune.SNN) && snn.built) || reuse.SNN){
-            save.SNN <- TRUE
-            if (reuse.SNN && !snn.built){
-              stop("No SNN stored to reuse.")
-            }
-            if (reuse.SNN && (!missing(genes.use) || !missing(pc.use) || !missing(k.param) || 
-                              !missing(k.scale) || !missing(prune.SNN))){
-              warning("SNN was not be rebuilt with new parameters. Continued with stored SNN. To suppress this
+#'
+FindClusters <- function(
+  object,
+  genes.use = NULL,
+  reduction.type = "pca",
+  pc.use = NULL,
+  k.param = 30,
+  k.scale = 25,
+  plot.SNN = FALSE,
+  prune.SNN = 1/15,
+  save.SNN = FALSE,
+  reuse.SNN = FALSE,
+  do.sparse = FALSE,
+  modularity.fxn = 1,
+  resolution = 0.8,
+  algorithm = 1,
+  n.start = 100,
+  n.iter = 10,
+  random.seed = 0,
+  print.output = TRUE,
+  temp.file.location = NULL,
+  distance.matrix = NULL
+) {
+  # for older objects without the snn.k slot
+  if(typeof(x = validObject(object = object, test = TRUE)) == "character") {
+    object@snn.k <- numeric()
+  }
+  snn.built <- FALSE
+  if (.hasSlot(object = object, name = "snn.dense")) {
+    if (length(x = object@snn.dense) > 1) {
+      snn.built <- TRUE
+    }
+  }
+  if (.hasSlot(object = object, name = "snn.sparse")) {
+    if (length(x = object@snn.sparse) > 1) {
+      snn.built <- TRUE
+    }
+  }
+  if ((
+    missing(x = genes.use) && missing(x = pc.use) && missing(x = k.param) &&
+    missing(x = k.scale) && missing(x = prune.SNN) && snn.built
+  ) || reuse.SNN) {
+    save.SNN <- TRUE
+    if (reuse.SNN && !snn.built) {
+      stop("No SNN stored to reuse.")
+    }
+    if (reuse.SNN && (
+      ! missing(x = genes.use) || ! missing(x = pc.use) || ! missing(x = k.param)
+      || ! missing(x = k.scale) || ! missing(x = prune.SNN)
+    )) {
+      warning("SNN was not be rebuilt with new parameters. Continued with stored SNN. To suppress this
                       warning, remove all SNN building parameters.")
-            }
-          }
-          # if any SNN building parameters are provided or it hasn't been built, build a new SNN
-          else{
-            object <- BuildSNN(object, genes.use, reduction.type, pc.use, k.param, k.scale,
-                               plot.SNN, prune.SNN, do.sparse, print.output,distance.matrix)
-          }
-          
-          # deal with sparse SNNs
-          if (length(object@snn.sparse) > 1) {
-            SNN.use <- object@snn.sparse
-          } else {
-            SNN.use <- object@snn.dense
-          }
-          for (r in resolution) {
-            object <- RunModularityClustering(object, SNN.use, modularity.fxn, r,
-                                              algorithm, n.start, n.iter, random.seed,
-                                              print.output, temp.file.location)
-            object <- GroupSingletons(object, SNN.use)
-            name <- paste("res.", r, sep = "")
-            object <- StashIdent(object, name)
-          }
-          
-          if (!save.SNN) {
-            object@snn.sparse <- sparseMatrix(1, 1, x = 1)
-            object@snn.dense <- matrix()
-            object@snn.k <- integer()
-          }
-          return(object)
-})
+    }
+  } else { # if any SNN building parameters are provided or it hasn't been built, build a new SNN
+    object <- BuildSNN(
+      object = object,
+      genes.use = genes.use,
+      reduction.type = reduction.type,
+      pc.use = pc.use,
+      k.param = k.param,
+      k.scale = k.scale,
+      plot.SNN = plot.SNN,
+      prune.SNN = prune.SNN,
+      do.sparse = do.sparse,
+      print.output = print.output,
+      distance.matrix = distance.matrix
+    )
+  }
+  # deal with sparse SNNs
+  if (length(x = object@snn.sparse) > 1) {
+    SNN.use <- object@snn.sparse
+  } else {
+    SNN.use <- object@snn.dense
+  }
+  for (r in resolution) {
+    object <- RunModularityClustering(
+      object = object,
+      SNN = SNN.use,
+      modularity = modularity.fxn,
+      resolution = r,
+      algorithm = algorithm,
+      n.start = n.start,
+      n.iter = n.iter,
+      random.seed = random.seed,
+      print.output = print.output,
+      temp.file.location = temp.file.location
+    )
+    object <- GroupSingletons(object = object, SNN = SNN.use)
+    name <- paste0("res.", r)
+    object <- StashIdent(object = object, save.name = name)
+  }
+  if (!save.SNN) {
+    object@snn.sparse <- sparseMatrix(1, 1, x = 1)
+    object@snn.dense <- matrix()
+    object@snn.k <- integer()
+  }
+  return(object)
+}
 
-
+# Documentation
+###############
 #' @export
-setGeneric("GetClusters", function(object) standardGeneric("GetClusters"))
-setMethod("GetClusters", signature="seurat",
-          function(object){
-            return(data.frame(object@ident))
-          }
-)
+#'
+GetClusters <- function(object) {
+  return(data.frame(object@ident))
+}
 
+# Documentation
+###############
 #' @export
-setGeneric("SetClusters", function(object, clusters=NULL)
-  standardGeneric("SetClusters"))
-setMethod("SetClusters", signature="seurat",
-          function(object, clusters = NULL){
-            cells.use <- rownames(clusters)
-            ident.use <- as.numeric(clusters[, 1])
-            object <- SetIdent(object, cells.use, ident.use)
-            return(object)
-          }
-)
+#'
+SetClusters <- function(object, clusters = NULL) {
+  cells.use <- rownames(x = clusters)
+  ident.use <- as.numeric(x = clusters[, 1])
+  object <- SetIdent(
+    object = object,
+    cells.use = cells.use,
+    ident.use = ident.use
+  )
+  return(object)
+}
 
+# Documentation
+###############
 #' @export
-setGeneric("SaveClusters", function(object, file)
-  standardGeneric("SaveClusters"))
-setMethod("SaveClusters", signature="seurat",
-          function(object, file){
-            my.clusters <- GetClusters(object)
-            write.table(my.clusters, file = file, sep="\t", quote = FALSE)
-          }
-)
+#'
+SaveClusters <- function(object, file) {
+  my.clusters <- GetClusters(object = object)
+  write.table(my.clusters, file = file, sep="\t", quote = FALSE)
+}
 
+
+# Documentation
+###############
 #' @export
-setGeneric("NumberClusters", function(object)
-  standardGeneric("NumberClusters"))
-setMethod("NumberClusters", signature="seurat",
-          function(object){
-            clusters <- unique(object@ident)
-            if (typeof(clusters) == "integer") {
-              n <- as.numeric(max(clusters)) + 1
-              for (i in clusters) {
-                object <- SetIdent(object, cells.use = WhichCells(object, i),
-                                    ident.use = n)
-                n <- n + 1
-              }
-              clusters <- unique(object@ident)
-            }
-            n <- 1
-            for (i in clusters) {
-              object <- SetIdent(object, cells.use = WhichCells(object, i),
-                                  ident.use = n)
-              n <- n + 1
-            }
-            return(object)
-          }
-)
+#'
+NumberClusters <- function(object) {
+  clusters <- unique(x = object@ident)
+  if (typeof(x = clusters) == "integer") {
+    n <- as.numeric(x = max(clusters)) + 1
+    for (i in clusters) {
+      object <- SetIdent(
+        object = object,
+        cells.use = WhichCells(object = object, ident = i),
+        ident.use = n
+      )
+      n <- n + 1
+    }
+    clusters <- unique(x = object@ident)
+  }
+  n <- 1
+  for (i in clusters) {
+    object <- SetIdent(
+      object,
+      cells.use = WhichCells(object = object, ident = i),
+      ident.use = n
+    )
+    n <- n + 1
+  }
+  return(object)
+}
 
-
-RunModularityClustering <- function(object, SNN = matrix(), modularity = 1,
-                                    resolution = 0.8, algorithm = 1,
-                                    n.start = 100, n.iter = 10, random.seed = 0,
-                                    print.output = TRUE, temp.file.location = NULL){
-  
-  seurat.dir <- system.file(package="Seurat")
-  
-  ModularityJarFile <- paste(seurat.dir,
-                             "/java/ModularityOptimizer.jar", sep = "")
-  
-  seurat.dir <- paste0(strsplit(seurat.dir, "/")[[1]][0:(length(strsplit(seurat.dir, "/")[[1]])-1)], collapse = "/")
-  seurat.dir <- paste0(seurat.dir, "/", sep = "")
-  
-  diag(SNN) <- 0
-  if (is.object(SNN)) {
-    SNN <- as(SNN, "dgTMatrix")
+# Documentation
+###############
+RunModularityClustering <- function(
+  object,
+  SNN = matrix(),
+  modularity = 1,
+  resolution = 0.8,
+  algorithm = 1,
+  n.start = 100,
+  n.iter = 10,
+  random.seed = 0,
+  print.output = TRUE,
+  temp.file.location = NULL
+) {
+  seurat.dir <- system.file(package = "Seurat")
+  ModularityJarFile <- paste0(seurat.dir, "/java/ModularityOptimizer.jar")
+  seurat.dir.base <- strsplit(x = seurat.dir, split = "/")[[1]]
+  seurat.dir <- paste0(
+    seurat.dir.base[0:(length(x = seurat.dir.base) - 1)],
+    collapse = "/"
+  )
+  seurat.dir <- paste0(seurat.dir, "/")
+  diag(x = SNN) <- 0
+  if (is.object(x = SNN)) {
+    SNN <- as(object = SNN, Class = "dgTMatrix")
     edge <- cbind(i = SNN@j, j = SNN@i, x = SNN@x)
   } else {
-    swap <- which(SNN != 0, arr.ind = TRUE) - 1
+    swap <- which(x = SNN != 0, arr.ind = TRUE) - 1
     temp <- swap[, 1]
     swap[, 1] <- swap[, 2]
     swap[, 2] <- temp
-    edge <- cbind(swap, SNN[which(SNN != 0, arr.ind = TRUE)])
+    edge <- cbind(swap, SNN[which(x = SNN != 0, arr.ind = TRUE)])
   }
-  rownames(edge) <- NULL
-  colnames(edge) <- NULL
-
-  edge <- edge[!duplicated(edge[, 1:2]), ]
-  
-  temp.file.location <- set.ifnull(temp.file.location, seurat.dir)
-  
-  unique_ID <- sample(10000 : 99999, 1)
-  edge_file <- paste(temp.file.location, "edge_", unique_ID, ".txt", sep = "")
-  output_file <- paste(temp.file.location, "output_", unique_ID, ".txt", sep = "")
+  rownames(x = edge) <- NULL
+  colnames(x = edge) <- NULL
+  edge <- edge[! duplicated(x = edge[, 1:2]), ]
+  temp.file.location <- set.ifnull(x = temp.file.location, y = seurat.dir)
+  unique_ID <- sample(x = 10000:99999, size = 1)
+  edge_file <- paste0(temp.file.location, "edge_", unique_ID, ".txt")
+  output_file <- paste0(temp.file.location, "output_", unique_ID, ".txt")
   while (file.exists(edge_file)) {
-    unique_ID <- sample(10000 : 99999, 1)
-    edge_file <- paste(temp.file.location, "edge_", unique_ID, ".txt", sep = "")
-    output_file <- paste(temp.file.location, "output", unique_ID, ".txt", sep = "")
+    unique_ID <- sample(x = 10000:99999, size = 1)
+    edge_file <- paste0(temp.file.location, "edge_", unique_ID, ".txt")
+    output_file <- paste0(temp.file.location, "output", unique_ID, ".txt")
   }
   if (print.output) {
     print.output <- 1
-  }
-  else {
+  } else {
     print.output <- 0
   }
-  
-  write.table(x = edge, file = edge_file, sep = "\t", row.names = FALSE,
-              col.names = FALSE)
-  if (modularity == 2 && resolution > 1){
+  write.table(
+    x = edge,
+    file = edge_file,
+    sep = "\t",
+    row.names = FALSE,
+    col.names = FALSE
+  )
+  if (modularity == 2 && resolution > 1) {
     stop("error: resolution<1 for alternative modularity")
   }
-  command <- paste("java -jar", shQuote(ModularityJarFile), shQuote(edge_file), shQuote(output_file),
-                   modularity, resolution, algorithm, n.start, n.iter,
-                   random.seed, print.output, sep = " ")
+  command <- paste(
+    "java -jar",
+    shQuote(string = ModularityJarFile),
+    shQuote(string = edge_file),
+    shQuote(string = output_file),
+    modularity,
+    resolution,
+    algorithm,
+    n.start,
+    n.iter,
+    random.seed,
+    print.output
+  )
   system(command, wait = TRUE)
   ident.use <- read.table(file = output_file, header = FALSE, sep = "\t")[, 1]
-  
-  object <- SetIdent(object, object@cell.names, ident.use)
+
+  object <- SetIdent(
+    object = object,
+    cells.use = object@cell.names,
+    ident.use = ident.use
+  )
   file.remove(edge_file)
   file.remove(output_file)
   return (object)
 }
 
-
-GroupSingletons <- function(object, SNN){
+# Documentation
+###############
+GroupSingletons <- function(object, SNN) {
   # identify singletons
   singletons <- c()
-  for (cluster in unique(object@ident)) {
-    if (length(WhichCells(object, cluster)) == 1) {
-      singletons <- append(singletons, cluster)
+  for (cluster in unique(x = object@ident)) {
+    if (length(x = WhichCells(object = object, ident = cluster)) == 1) {
+      singletons <- append(x = singletons, values = cluster)
     }
   }
   # calculate connectivity of singletons to other clusters, add singleton
   # to cluster it is most connected to
-  cluster_names <- unique(object@ident)
-  cluster_names <- setdiff(cluster_names, singletons)
-  connectivity <- vector(mode="numeric", length = length(cluster_names))
-  names(connectivity) <- cluster_names
+  cluster_names <- unique(x = object@ident)
+  cluster_names <- setdiff(x = cluster_names, y = singletons)
+  connectivity <- vector(mode="numeric", length = length(x = cluster_names))
+  names(x = connectivity) <- cluster_names
   for (i in singletons) {
     for (j in cluster_names) {
-      subSNN = SNN[WhichCells(object, i), match(WhichCells(object, j), colnames(SNN))]
-      if (is.object(subSNN)) {
-        connectivity[j] <- sum(subSNN) / (nrow(subSNN) * ncol(subSNN))
+      subSNN = SNN[
+        WhichCells(object = object, ident = i), # Row
+        match(
+          x = WhichCells(object = object, ident = j),
+          table = colnames(x = SNN)
+        ) # Column
+      ]
+      if (is.object(x = subSNN)) {
+        connectivity[j] <- sum(subSNN) / (nrow(x = subSNN) * ncol(x = subSNN))
       } else {
-        connectivity[j] <- mean(subSNN)
+        connectivity[j] <- mean(x = subSNN)
       }
     }
     m <- max(connectivity, na.rm = T)
-    mi <- which(connectivity == m, arr.ind = TRUE)
-    closest_cluster <- sample(names(connectivity[mi]), 1)
-    object <- SetIdent(object, cells.use = WhichCells(object,i), 
-                        ident.use = closest_cluster)
-    
+    mi <- which(x = connectivity == m, arr.ind = TRUE)
+    closest_cluster <- sample(x = names(x = connectivity[mi]), 1)
+    object <- SetIdent(
+      object = object,
+      cells.use = WhichCells(object = object, ident = i),
+      ident.use = closest_cluster
+    )
   }
-  if (length(singletons) > 0){
-    print(paste(length(singletons), "singletons identified.", length(unique(object@ident)), "final clusters."))
+  if (length(x = singletons) > 0) {
+    print(paste(
+      length(x = singletons),
+      "singletons identified.",
+      length(x = unique(object@ident)),
+      "final clusters."
+    ))
   }
   return(object)
 }
