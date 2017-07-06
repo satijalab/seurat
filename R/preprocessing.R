0afd7816b36c7e407c0e37920da1a297cc4ae9f2
Paul Hoffman <hoff0792@umn.edu>
2017-07-06 14:58:24 -0400

    Move RegressOut and temporarily deal with the two RegressOutNBreg functions...

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -397,3 +397,141 @@ SampleUMI <- function(
     )
   )
 }
+
+#' Regress out technical effects and cell cycle
+#'
+#' Remove unwanted effects from scale.data
+#'
+#' @param object Seurat object
+#' @param latent.vars effects to regress out
+#' @param genes.regress gene to run regression for (default is all genes)
+#' @param model.use Use a linear model or generalized linear model (poisson, negative binomial) for the regression. Options are 'linear' (default), 'poisson', and 'negbinom'
+#' @param use.umi Regress on UMI count data. Default is FALSE for linear modeling, but automatically set to TRUE if model.use is 'negbinom' or 'poisson'
+#' @param do.scale Whether to scale the data.
+#' @param do.center Whether to center the data.
+#' @param scale.max Max value to accept for scaled data. The default is 10. Setting this can help
+#' reduce the effects of genes that are only expressed in a very small number of cells.
+#'
+#' @return Returns Seurat object with the scale.data (object@scale.data) genes returning the residuals from the regression model
+#'
+#' @import Matrix
+#'
+#' @export
+#'
+RegressOut <- function(
+  object,
+  latent.vars,
+  genes.regress = NULL,
+  model.use = 'linear',
+  use.umi = FALSE,
+  do.scale = TRUE,
+  do.center = TRUE,
+  scale.max = 10
+) {
+  possible.models <- c("linear", "poisson", "negbinom")
+  if (! model.use %in% possible.models){
+    stop(
+      paste0(
+        model.use,
+        " is not a valid model. Please use one the following: ",
+        paste0(possible.models, collapse = ", "),
+        "."
+      )
+    )
+  }
+  genes.regress <- set.ifnull(genes.regress,rownames(object@data))
+  genes.regress <- ainb(genes.regress,rownames(object@data))
+  latent.data <- FetchData(object = object, vars.all = latent.vars)
+  bin.size <- 100
+  if (model.use == 'negbinom') {
+    bin.size <- 5
+  }
+  bin.ind <- ceiling(x = 1:length(x = genes.regress) / bin.size)
+  max.bin <- max(bin.ind)
+  print(paste("Regressing out", latent.vars))
+  pb <- txtProgressBar(min = 0, max = max.bin, style = 3)
+  data.resid <- c()
+  data.use <- object@data[genes.regress, , drop = FALSE];
+  if (model.use != "linear") {
+    use.umi <- TRUE
+  }
+  if (use.umi) {
+    data.use <- object@raw.data[genes.regress, object@cell.names, drop = FALSE]
+  }
+  for (i in 1:max.bin) {
+    genes.bin.regress <- rownames(x = data.use)[bin.ind == i]
+    gene.expr <- as.matrix(x = data.use[genes.bin.regress, , drop = FALSE])
+    new.data <- do.call(
+      rbind,
+      lapply(
+        X = genes.bin.regress,
+        FUN = function(x) {
+          regression.mat <- cbind(latent.data, gene.expr[x,])
+          colnames(x = regression.mat) <- c(colnames(x = latent.data), "GENE")
+          fmla <- as.formula(
+            object = paste0(
+              "GENE ",
+              " ~ ",
+              paste(latent.vars, collapse = "+")
+            )
+          )
+          if (model.use == 'linear') {
+            return(lm(formula = fmla, data = regression.mat)$residuals)
+          }
+          if (model.use == 'poisson') {
+            return(residuals(
+              object = glm(
+                formula = fmla,
+                data = regression.mat,
+                family = "poisson"
+              ),
+              type='pearson'
+            ))
+          }
+          if (model.use == 'negbinom') {
+            return(nb.residuals(
+              formula = fmla,
+              regression.mat = regression.mat,
+              gene = x
+            ))
+          }
+        }
+      )
+    )
+    if (i == 1) {
+      data.resid=new.data
+    }
+    if (i > 1) {
+      data.resid=rbind(data.resid,new.data)
+    }
+    setTxtProgressBar(pb, i)
+  }
+  close(pb)
+  rownames(x = data.resid) <- genes.regress
+  if (use.umi) {
+    data.resid <- log1p(
+      x = sweep(
+        x = data.resid,
+        MARGIN = 1,
+        STATS = apply(X = data.resid, MARGIN = 1, FUN = min),
+        FUN = "-"
+      )
+    )
+  }
+  object@scale.data <- data.resid
+  if (do.scale) {
+    if (use.umi && base::missing(x = scale.max)) {
+      scale.max <- 50
+    }
+    object <- ScaleData(
+      object = object,
+      genes.use = rownames(x = data.resid),
+      data.use = data.resid,
+      do.center = do.center,
+      do.scale = do.scale,
+      scale.max = scale.max
+    )
+  }
+  object@scale.data[is.na(x = object@scale.data)] <- 0
+  return(object)
+}
