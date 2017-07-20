18908cf17f37d950280c130cc49f2c45b98254fb
Andrew Butler <andrew.butler33@gmail.com>
2017-07-20 11:16:55 -0400

    consistent thresholding between WhichCells and SubsetData

--- R/interaction.R
+++ R/interaction.R
@@ -681,7 +681,7 @@ WhichCells <- function(
     if(! is.null(x = accept.value)) {
       pass.inds <- which(x = subset.data == accept.value)
     } else {
-      pass.inds <- which(x = (subset.data >= accept.low) & (subset.data <= accept.high))
+      pass.inds <- which(x = (subset.data > accept.low) & (subset.data < accept.high))
     }
     cells.use <- rownames(x = data.use)[pass.inds]
   }
