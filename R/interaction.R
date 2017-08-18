d03baae08173e3dbe39dee492c8cc719ced26a4e
Paul Hoffman <phoffman@nygenome.org>
2017-08-18 14:28:03 -0400

    Fix errors in examples

--- R/interaction.R
+++ R/interaction.R
@@ -38,12 +38,12 @@ globalVariables(names = 'cell.name', package = 'Seurat', add = TRUE)
 #'
 #' @examples
 #' # Split pbmc_small for this example
-#' pbmc1 <- SubsetData(object = pbmc_small, cell.names = pbmc_small@cell.names[1:40])
+#' pbmc1 <- SubsetData(object = pbmc_small, cells.use = pbmc_small@cell.names[1:40])
 #' pbmc1
-#' pbmc2 <- SubsetData(object = pbmc_small, cell.names = pbmc_small@cell.names[41:80])
+#' pbmc2 <- SubsetData(object = pbmc_small, cells.use = pbmc_small@cell.names[41:80])
 #' pbmc2
 #' # Merge pbmc1 and pbmc2 into one Seurat object
-#' pbmc_merged <- MergeSeurat(object1 = pmbc1, object2 = pbmc2)
+#' pbmc_merged <- MergeSeurat(object1 = pbmc1, object2 = pbmc2)
 #' pbmc_merged
 #'
 MergeSeurat <- function(
@@ -341,7 +341,7 @@ AddSamples <- function(
 #' @export
 #'
 #' @examples
-#' pbmc1 <- SubsetData(object = pbmc_small, cell.names = pbmc_small@cell.names[1:40])
+#' pbmc1 <- SubsetData(object = pbmc_small, cells.use = pbmc_small@cell.names[1:40])
 #' pbmc1
 #'
 SubsetData <- function(
