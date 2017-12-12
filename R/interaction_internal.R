8bb9a1969ccea269007591af3092407d664e8a2a
Andrew Butler <andrew.butler33@gmail.com>
2017-12-12 17:44:15 -0500

    add empty function for downsampling off a given matrix

---
+++ R/interaction_internal.R
@@ -0,0 +1,10 @@
+# Downsamples a matrix, returns vector of row (cell) names
+#
+# @param matrix
+# @param downsample.options
+
+DownsampleMatrix <- function(mat, ...) {
+  # super cool downsampling code here
+  cells <- vector()
+  return(cells)
+}
