b490217bbdca21fea2e564b1277d0d64271eae1f
Isaac Virshup <ivirshup@gmail.com>
2018-06-01 13:31:55 +1000

    Fixed conversion error for seurat with no dr to anndata
    Allowed conversion of `seurat` object to `anndata` by setting argument `obsm` to `NULL` if no dimensionality reduction is present. Additionally added "this shouldn't throw an error" tests.

---
+++ tests/testthat/test_conversion.R
@@ -0,0 +1,14 @@
+# Tests for conversions
+set.seed(42)
+
+context("conversions")
+
+test_that("Conversion to anndata", {
+  expect_silent(Convert(pbmc_small, "anndata"))
+})
+
+test_that("Conversion to anndata, no dr", {
+  nodr <- pbmc_small
+  nodr@dr <- list()
+  expect_silent(Convert(nodr, "anndata"))
+})
