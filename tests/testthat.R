c4840c8958083bdea83c3682ee6ac478973bd7b5
Isaac Virshup <ivirshup@gmail.com>
2018-06-14 14:37:39 +1000

    🤞 Install deps using reticulate

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,11 @@
+if (Sys.getenv("CI")=="true") {
+  library(reticulate)
+  conda_create("rettest")
+  conda_install("rettest", c("python=3.6", "numpy", "seaborn", "scikit-learn", "statsmodels", "numba", "cython"), forge=F)
+  conda_install("rettest", c("scanpy", "phate"), pip=T, pip_ignore_installed=F)
+  use_condaenv("rettest")
+}
+
 library(testthat)
 library(Seurat)
 
--- travis_setup.sh
+++ travis_setup.sh
@@ -14,7 +14,7 @@ fi
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh 
 fi
 
 bash miniconda.sh -b -p $HOME/miniconda
@@ -26,11 +26,11 @@ conda update -q conda
 # source activate base
 conda info -a
 # Scanpy dependencies
-conda install -q numpy seaborn scikit-learn statsmodels numba
-pip install --upgrade pip
-pip install scanpy
-pip install phate
-echo "check python installation"
-pip list
-conda list
-which python
+conda install -q numpy # seaborn scikit-learn statsmodels numba
+# pip install --upgrade pip
+# pip install scanpy
+# pip install phate
+# echo "check python installation"
+# pip list
+# conda list
+# which python
