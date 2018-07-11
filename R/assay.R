26ee2d6928c5ee1c5207581029bfa8e78ae123a1
Paul Hoffman <phoffman@nygenome.org>
2018-07-11 11:29:34 -0400

    Update check for SetAssayData: allow scale.data to have fewer features than data/raw.data

--- R/assay.R
+++ R/assay.R
@@ -71,8 +71,13 @@ SetAssayData.Assay <- function(object, slot, new.data) {
   if (!slot %in% slots.use) {
     stop("'slot' must be one of ", paste(slots.use, collapse = ', '))
   }
-  if (any(dim(x = new.data) != dim(x = object))) {
-    stop("The new data doesn't have the same dimensions as the current data")
+  if (ncol(x = new.data) != ncol(x = object)) {
+    stop("The new data doesn't have the same number of cells as the current data")
+  }
+  if (slot == 'scale.data' && nrow(x = new.data) > nrow(x = object)) {
+    stop("Cannot add more features than present in current data")
+  } else if (slot != 'scale.data' && nrow(x = new.data) != nrow(x = object)) {
+    stop("The new data doesn't have the same number of features as the current data")
   }
   new.cells <- colnames(x = new.data)
   if (!all(new.cells %in% colnames(x = object))) {
