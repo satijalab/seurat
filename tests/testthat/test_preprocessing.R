48171ff191c98dc5d282d101d52a3bbcc6f966fb
Andrew Butler <abutler@nygenome.org>
2018-08-27 13:08:16 -0700

    update tests

--- tests/testthat/test_preprocessing.R
+++ tests/testthat/test_preprocessing.R
@@ -134,7 +134,7 @@ test_that("Negative binomial regression works as expected", {
 })
 
 test_that("Regression error handling checks out", {
-  expect_error(ScaleData(object, vars.to.regress = "nUMI", model.use = "not.a.model"))
+  expect_error(ScaleData(object, vars.to.regress = "nCount_RNA", model.use = "not.a.model"))
 })
 
 object <- ScaleData(
@@ -218,31 +218,31 @@ context("FilterCells")
 
 object.filtered <- FilterCells(
   object = object,
-  subset.names = c("nFeature_RNA", "nUMI"),
+  subset.names = c("nFeature_RNA", "nCount_RNA"),
   low.thresholds = c(20, 100)
 )
 
 test_that("FilterCells low thresholds work properly", {
   expect_equal(ncol(x = object.filtered), 62)
   expect_true(!any(object.filtered["nFeature_RNA"] < 20))
-  expect_true(!any(object.filtered["nUMI"] < 100))
+  expect_true(!any(object.filtered["nCount_RNA"] < 100))
 })
 
 object.filtered <- FilterCells(
   object = object,
-  subset.names = c("nFeature_RNA", "nUMI"),
+  subset.names = c("nFeature_RNA", "nCount_RNA"),
   high.thresholds = c(50, 300)
 )
 
 test_that("FilterCells high thresholds work properly", {
   expect_equal(ncol(x = object.filtered), 35)
   expect_true(!any(object.filtered["nFeature_RNA"] > 50))
-  expect_true(!any(object.filtered["nUMI"] > 300))
+  expect_true(!any(object.filtered["nCount_RNA"] > 300))
 })
 
 test_that("FilterCells handles input correctly", {
-  expect_error(FilterCells(object, subset.names = c("nGene", "nUMI"), high.thresholds = 30))
-  expect_error(FilterCells(object, subset.names = c("nGene", "nUMI"), low.thresholds = 20))
+  expect_error(FilterCells(object, subset.names = c("nGene", "nCount_RNA"), high.thresholds = 30))
+  expect_error(FilterCells(object, subset.names = c("nGene", "nCount_RNA"), low.thresholds = 20))
   expect_error(FilterCells(object, subset.names = c("nGene"), high.thresholds = c(30, 300)))
 })
 
