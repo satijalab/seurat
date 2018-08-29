f09b67be0b2721fa37decbc87217f856451997c6
Paul Hoffman <phoffman@nygenome.org>
2018-08-29 15:25:35 -0400

    Get CellCycleScoring working for Seurat v3.X

--- R/utilities.R
+++ R/utilities.R
@@ -6,26 +6,27 @@ NULL
 # Functions
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-#' Calculate module scores for gene expression programs in single cells
+#' Calculate module scores for featre expression programs in single cells
 #'
 #' Calculate the average expression levels of each program (cluster) on single cell level,
-#' subtracted by the aggregated expression of control gene sets.
-#' All analyzed genes are binned based on averaged expression, and the control genes are
+#' subtracted by the aggregated expression of control featre sets.
+#' All analyzed featres are binned based on averaged expression, and the control featres are
 #' randomly selected from each bin.
 #'
 #' @param object Seurat object
-#' @param genes.list Gene expression programs in list
-#' @param genes.pool List of genes to check expression levels agains, defaults to rownames(x = object@data)
-#' @param n.bin Number of bins of aggregate expression levels for all analyzed genes
-#' @param seed.use Random seed for sampling
-#' @param ctrl.size Number of control genes selected from the same bin per analyzed gene
-#' @param use.k Use gene clusters returned from DoKMeans()
-#' @param enrich.name Name for the expression programs
-#' @param random.seed Set a random seed
+#' @param features Featre expression programs in list
+#' @param pool List of features to check expression levels agains, defaults to \code{rownames(x = object)}
+#' @param nbin Number of bins of aggregate expression levels for all analyzed features
+#' @param ctrl Number of control features selected from the same bin per analyzed feature
+#' @param k Use feature clusters returned from DoKMeans
+#' @param assay Name of assay to use
+#' @param name Name for the expression programs
+#' @param seed Set a random seed
 #'
-#' @return Returns a Seurat object with module scores added to object@meta.data
+#' @return Returns a Seurat object with module scores added to object meta data
 #'
-#' @importFrom Hmisc cut2
+# @importFrom Hmisc cut2
+#' @importFrom ggplot2 cut_number
 #' @importFrom Matrix rowMeans colMeans
 #'
 #' @references Tirosh et al, Science (2016)
@@ -34,7 +35,7 @@ NULL
 #'
 #' @examples
 #' \dontrun{
-#' cd_genes <- list(c(
+#' cd_features <- list(c(
 #'   'CD79B',
 #'   'CD79A',
 #'   'CD19',
@@ -53,80 +54,83 @@ NULL
 #' ))
 #' pbmc_small <- AddModuleScore(
 #'   object = pbmc_small,
-#'   genes.list = cd_genes,
-#'   ctrl.size = 5,
-#'   enrich.name = 'CD_Genes'
+#'   features = cd_features,
+#'   ctrl = 5,
+#'   name = 'CD_Features'
 #' )
-#' head(x = pbmc_small@meta.data)
+#' head(x = pbmc_small[])
 #' }
+#'
 AddModuleScore <- function(
   object,
-  genes.list = NULL,
-  genes.pool = NULL,
-  n.bin = 25,
-  seed.use = 1,
-  ctrl.size = 100,
-  use.k = FALSE,
-  enrich.name = "Cluster",
-  random.seed = 1
+  features,
+  pool = NULL,
+  nbin = 24,
+  ctrl = 100,
+  k = FALSE,
+  assay = NULL,
+  name = 'Cluster',
+  seed = 1
 ) {
-  set.seed(seed = random.seed)
-  genes.old <- genes.list
-  if (use.k) {
-    genes.list <- list()
+  set.seed(seed = seed)
+  assay.old <- DefaultAssay(object = object)
+  assay <- assay %||% assay.old
+  DefaultAssay(object = object) <- assay
+  assay.data <- GetAssayData(object = object)
+  features.old <- features
+  if (k) {
+    .NotYetUsed(arg = 'k')
+    features <- list()
     for (i in as.numeric(x = names(x = table(object@kmeans.obj[[1]]$cluster)))) {
-      genes.list[[i]] <- names(x = which(x = object@kmeans.obj[[1]]$cluster == i))
+      features[[i]] <- names(x = which(x = object@kmeans.obj[[1]]$cluster == i))
     }
-    cluster.length <- length(x = genes.list)
+    cluster.length <- length(x = features)
   } else {
-    if (is.null(x = genes.list)) {
-      stop("Missing input gene list")
+    if (is.null(x = features)) {
+      stop("Missing input feature list")
     }
-    genes.list <- lapply(
-      X = genes.list,
+    features <- lapply(
+      X = features,
       FUN = function(x) {
-        return(intersect(x = x, y = rownames(x = object@data)))
+        return(intersect(x = x, y = rownames(x = object)))
       }
     )
-    cluster.length <- length(x = genes.list)
+    cluster.length <- length(x = features)
   }
-  if (!all(LengthCheck(values = genes.list))) {
+  if (!all(LengthCheck(values = features))) {
     warning(paste(
-      'Could not find enough genes in the object from the following gene lists:',
-      paste(names(x = which(x = ! LengthCheck(values = genes.list)))),
+      'Could not find enough features in the object from the following feature lists:',
+      paste(names(x = which(x = !LengthCheck(values = features)))),
       'Attempting to match case...'
     ))
-    genes.list <- lapply(
-      X = genes.old,
-      FUN = CaseMatch, match = rownames(x = object@data)
+    features <- lapply(
+      X = features.old,
+      FUN = CaseMatch,
+      match = rownames(x = object)
     )
   }
-  if (!all(LengthCheck(values = genes.list))) {
+  if (!all(LengthCheck(values = features))) {
     stop(paste(
-      'The following gene lists do not have enough genes present in the object:',
-      paste(names(x = which(x = ! LengthCheck(values = genes.list)))),
+      'The following feature lists do not have enough features present in the object:',
+      paste(names(x = which(x = !LengthCheck(values = features)))),
       'exiting...'
     ))
   }
-  if (is.null(x = genes.pool)) {
-    genes.pool = rownames(x = object@data)
-  }
-  data.avg <- Matrix::rowMeans(x = object@data[genes.pool, ])
+  pool <- pool %||% rownames(x = object)
+  data.avg <- Matrix::rowMeans(x = assay.data[pool, ])
   data.avg <- data.avg[order(data.avg)]
-  data.cut <- as.numeric(x = Hmisc::cut2(
-    x = data.avg,
-    m = round(x = length(x = data.avg) / n.bin)
-  ))
+  data.cut <- cut_number(x = data.avg, n = nbin, labels = FALSE, right = FALSE)
+  # data.cut <- as.numeric(x = Hmisc::cut2(x = data.avg, m = round(x = length(x = data.avg) / (nbin + 1))))
   names(x = data.cut) <- names(x = data.avg)
   ctrl.use <- vector(mode = "list", length = cluster.length)
   for (i in 1:cluster.length) {
-    genes.use <- genes.list[[i]]
-    for (j in 1:length(x = genes.use)) {
+    features.use <- features[[i]]
+    for (j in 1:length(x = features.use)) {
       ctrl.use[[i]] <- c(
         ctrl.use[[i]],
         names(x = sample(
-          x = data.cut[which(x = data.cut == data.cut[genes.use[j]])],
-          size = ctrl.size,
+          x = data.cut[which(x = data.cut == data.cut[features.use[j]])],
+          size = ctrl,
           replace = FALSE
         ))
       )
@@ -136,32 +140,29 @@ AddModuleScore <- function(
   ctrl.scores <- matrix(
     data = numeric(length = 1L),
     nrow = length(x = ctrl.use),
-    ncol = ncol(x = object@data)
+    ncol = ncol(x = object)
   )
   for (i in 1:length(ctrl.use)) {
-    genes.use <- ctrl.use[[i]]
-    ctrl.scores[i, ] <- Matrix::colMeans(x = object@data[genes.use, ])
+    features.use <- ctrl.use[[i]]
+    ctrl.scores[i, ] <- Matrix::colMeans(x = assay.data[features.use, ])
   }
-  genes.scores <- matrix(
+  features.scores <- matrix(
     data = numeric(length = 1L),
     nrow = cluster.length,
-    ncol = ncol(x = object@data)
+    ncol = ncol(x = object)
   )
   for (i in 1:cluster.length) {
-    genes.use <- genes.list[[i]]
-    data.use <- object@data[genes.use, , drop = FALSE]
-    genes.scores[i, ] <- Matrix::colMeans(x = data.use)
+    features.use <- features[[i]]
+    data.use <- assay.data[features.use, , drop = FALSE]
+    features.scores[i, ] <- Matrix::colMeans(x = data.use)
   }
-  genes.scores.use <- genes.scores - ctrl.scores
-  rownames(x = genes.scores.use) <- paste0(enrich.name, 1:cluster.length)
-  genes.scores.use <- as.data.frame(x = t(x = genes.scores.use))
-  rownames(x = genes.scores.use) <- colnames(x = object@data)
-  object <- AddMetaData(
-    object = object,
-    metadata = genes.scores.use,
-    col.name = colnames(x = genes.scores.use)
-  )
+  features.scores.use <- features.scores - ctrl.scores
+  rownames(x = features.scores.use) <- paste0(name, 1:cluster.length)
+  features.scores.use <- as.data.frame(x = t(x = features.scores.use))
+  rownames(x = features.scores.use) <- colnames(x = object)
+  object[colnames(x = features.scores.use)] <- features.scores.use
   gc(verbose = FALSE)
+  DefaultAssay(object = object) <- assay.old
   return(object)
 }
 
@@ -338,12 +339,12 @@ CaseMatch <- function(search, match) {
 #' Score cell cycle phases
 #'
 #' @param object A Seurat object
-#' @param g2m.genes A vector of genes associated with G2M phase
-#' @param s.genes A vector of genes associated with S phases
+#' @param s.features A vector of features associated with S phase
+#' @param g2m.features A vector of features associated with G2M phase
 #' @param set.ident If true, sets identity to phase assignments
 #' Stashes old identities in 'old.ident'
 #'
-#' @return A Seurat object with the following columns added to object@meta.data: S.Score, G2M.Score, and Phase
+#' @return A Seurat object with the following columns added to object meta data: S.Score, G2M.Score, and Phase
 #'
 #' @seealso \code{AddModuleScore}
 #'
@@ -364,20 +365,20 @@ CaseMatch <- function(search, match) {
 #'
 CellCycleScoring <- function(
   object,
-  g2m.genes,
-  s.genes,
+  s.features,
+  g2m.features,
   set.ident = FALSE
 ) {
-  enrich.name <- 'Cell Cycle'
-  genes.list <- list('S.Score' = s.genes, 'G2M.Score' = g2m.genes)
+  name <- 'Cell Cycle'
+  features <- list('S.Score' = s.features, 'G2M.Score' = g2m.features)
   object.cc <- AddModuleScore(
     object = object,
-    genes.list = genes.list,
-    enrich.name = enrich.name,
-    ctrl.size = min(vapply(X = genes.list, FUN = length, FUN.VALUE = numeric(1)))
+    features = features,
+    name = name,
+    ctrl = min(vapply(X = features, FUN = length, FUN.VALUE = numeric(length = 1)))
   )
-  cc.columns <- grep(pattern = enrich.name, x = colnames(x = object.cc@meta.data))
-  cc.scores <- object.cc@meta.data[, cc.columns]
+  cc.columns <- grep(pattern = name, x = colnames(x = object.cc[]), value = TRUE)
+  cc.scores <- object.cc[cc.columns]
   rm(object.cc)
   gc(verbose = FALSE)
   assignments <- apply(
@@ -399,10 +400,10 @@ CellCycleScoring <- function(
   colnames(x = cc.scores) <- c('rownames', 'S.Score', 'G2M.Score', 'Phase')
   rownames(x = cc.scores) <- cc.scores$rownames
   cc.scores <- cc.scores[, c('S.Score', 'G2M.Score', 'Phase')]
-  object <- AddMetaData(object = object, metadata = cc.scores)
+  object[colnames(x = cc.scores)] <- cc.scores
   if (set.ident) {
-    object <- StashIdent(object = object, save.name = 'old.ident')
-    object <- SetAllIdent(object = object, id = 'Phase')
+    object['old.ident'] <- Idents(object = object)
+    Idents(object = object) <- 'Phase'
   }
   return(object)
 }
