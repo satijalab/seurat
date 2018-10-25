2f47427da763307712e1b51286b0f6b6e1d52568
k3yavi <avi.srivastava@stonybrook.edu>
2018-10-25 15:28:40 -0400

    adding alevin loading files

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -282,6 +282,54 @@ Read10X_h5 <- function(filename, ensg.names = FALSE){
   }
 }
 
+#' Load in data from Alevin pipeline
+#'
+#' Enables easy loading of csv format matrix provided by Alevin
+# ran with `--dumpCsvCounts` flags.
+#'
+#' @param data.dir Directory containing the alevin/quant_mat*
+#' files provided by Alevin.
+#'
+#' @return Returns a matrix with rows and columns labeled
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' data_dir <- 'path/to/output/directory'
+#' list.files(data_dir) # Should show alevin/quants_mat* files
+#' expression_matrix <- ReadAlevin(base.path = data_dir)
+#' seurat_object = CreateSeuratObject(raw.data = expression_matrix)
+#' }
+#'
+
+ReadAlevin <- function( base.path = NULL ){
+    if (! dir.exists(base.path )){
+      stop("Directory provided does not exist")
+    }
+
+    barcode.loc <- paste0( base.path, "alevin/quants_mat_rows.txt" )
+    gene.loc <- paste0( base.path, "alevin/quants_mat_cols.txt" )
+    matrix.loc <- paste0( base.path, "alevin/quants_mat.csv" )
+    if (!file.exists( barcode.loc )){
+      stop("Barcode file missing")
+    }
+    if (! file.exists(gene.loc) ){
+      stop("Gene name file missing")
+    }
+    if (! file.exists(matrix.loc )){
+      stop("Expression matrix file missing")
+    }
+    matrix <- t(as.matrix(read.csv( matrix.loc, header=FALSE))[,-(ncol(matrix)-1)])
+
+    cell.names <- readLines( barcode.loc )
+    gene.names <- readLines( gene.loc )
+
+    colnames(matrix) <- cell.names
+    rownames(matrix) <- gene.names
+    matrix[is.na(matrix)] <- 0
+    return(matrix)
+}
 
 #' Normalize Assay Data
 #'
