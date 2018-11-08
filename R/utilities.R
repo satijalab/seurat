c58f2e2c1868acecc47e2e166260392c9aa0d8ce
Nikolay Markov <a4blank@yahoo.com>
2018-11-08 17:46:11 +0300

    ExportToCellbrowser function

--- R/utilities.R
+++ R/utilities.R
@@ -1148,3 +1148,154 @@ ToNumeric <- function(x){
   }
   return(x)
 }
+
+#' Export Seurat object for UCSC cell browser
+#'
+#' @param object Seurat object
+#' @param dir output directory path
+#' @param dataset.name name of the dataset
+#' @param meta.fields vector of metadata fields to export
+#' @param meta.fields.names list that defines metadata field names
+#'                          after the export. Should map metadata
+#'                          column name to export name
+#' @param embeddings vector of embedding names to export
+#' @param markers.file path to file with marker genes
+#' @param cluster.field name of the metadata field containing cell cluster
+#' @param port on which port to run UCSC cell browser after export
+#'
+#' @export
+#'
+#' @examples
+#' ExportToCellbrowser(object = pbmc_small, dataset.name = "PBMC", dir = "out")
+#'
+ExportToCellbrowser <- function(
+  object,
+  dir = NULL,
+  dataset.name = NULL,
+  meta.fields = NULL,
+  meta.fields.names = NULL,
+  embeddings = c("tsne"),
+  markers.file = NULL,
+  cluster.field = NULL,
+  port = NULL
+) {
+  if (is.null(dir)) {
+    stop("Missing output directory")
+  }
+  if (is.null(dataset.name)) {
+    stop("Missing dataset name parameter")
+  }
+  if (is.null(meta.fields)) {
+    meta.fields <- c("nCount_RNA", "nFeature_RNA")
+    if (length(levels(x = Idents(object = object))) > 1) {
+      meta.fields <- c(meta.fields, ".ident")
+    }
+  }
+  if (!dir.exists(dir)) {
+    dir.create(dir)
+  }
+  if (!dir.exists(dir)) {
+    stop(sprintf("Output director %s cannot be created or is a file", dir))
+  }
+
+  order <- Cells(object = object)
+  enum.fields <- c()
+
+  # Export expression matrix
+  df <- as.data.frame(as.matrix(GetAssayData(object = object)))
+  df <- data.frame(gene=rownames(object), df)
+  z <- gzfile(file.path(dir, "exprMatrix.tsv.gz"))
+  write.table(df, sep="\t", file=z, quote = FALSE, row.names = FALSE)
+
+  # Export cell embeddings
+  embeddings.conf <- c()
+  for (embedding in embeddings) {
+    df <- Embeddings(object = object, reduction = embedding)
+    if (ncol(df) > 2) {
+      warning(sprintf(
+        'Embedding %s has more than 2 coordinates, taking only the first 2',
+        embedding
+      ))
+      df <- df[, 1:2]
+    }
+    colnames(df) <- c("x", "y")
+    df <- data.frame(cellId = rownames(df), df)
+    fname <- file.path(
+      dir,
+      sprintf("%s.coords.tsv", embedding)
+    )
+    write.table(df[order, ], sep="\t", file=fname, quote = FALSE, row.names = FALSE)
+    conf <- sprintf(
+      '{"file": "%s.coords.tsv", "shortLabel": "Seurat %1$s"}',
+      embedding
+    )
+    embeddings.conf <- c(embeddings.conf, conf)
+  }
+
+  # Export metadata
+  df <- data.frame(row.names = rownames(object[[]]))
+  for (field in meta.fields) {
+    if (field == ".ident") {
+      df$Cluster <- Idents(object = object)
+      enum.fields <- c(enum.fields, "Cluster")
+      cluster.field <- "Cluster"
+    } else {
+      name <- meta.fields.names[[field]]
+      if (is.null(name)) {
+        name <- field
+      }
+      df[[name]] <- object[[field]][, 1]
+      if (!is.numeric(df[[name]])) {
+        enum.fields <- c(enum.fields, name)
+      }
+    }
+  }
+  df <- data.frame(Cell=rownames(df), df)
+  write.table(df[order, ], sep="\t", file=file.path(dir, "meta.tsv"), quote = FALSE, row.names=FALSE)
+
+  # Export markers
+  markers.string <- ''
+  if (!is.null(markers.file)) {
+    file.copy(markers.file, file.path(dir, "markers.tsv"))
+    markers.string <- 'markers = [{"file": "markers.tsv", "shortLabel": "Seurat Cluster Markers"}]'
+  }
+
+  config <- 'name="%s"
+shortLabel="%1$s"
+exprMatrix="exprMatrix.tsv.gz"
+#tags = ["10x", "smartseq2"]
+meta="meta.tsv"
+# possible values: "gencode-human", "gencode-mouse", "symbol"
+geneIdType="auto"
+clusterField="%s"
+labelField="%2$s"
+enumFields=%s
+%s
+coords=%s'
+  enum.string <- paste0(
+    "[",
+    paste(paste0('"', enum.fields, '"'), collapse = ", "),
+    "]"
+  )
+  coords.string <- paste0(
+    "[",
+    paste(embeddings.conf, collapse = ",\n"),
+    "]"
+  )
+  config <- sprintf(
+    config,
+    dataset.name,
+    cluster.field,
+    enum.string,
+    markers.string,
+    coords.string
+  )
+  cat(config, file=file.path(dir, "cellbrowser.conf"))
+
+  if (!is.null(port) && require(reticulate)) {
+    library(reticulate)
+    py_install("cellbrowser") # see https://rstudio.github.io/reticulate/articles/python_packages.html
+    cb <- import("cellbrowser") # see https://github.com/rstudio/reticulate/issues/73
+    cb$cellbrowser$cbBuild("cellbrowser.conf", dir, port)
+  }
+}
