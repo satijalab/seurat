b5794a9b0c71a89a0b5f12131c53f4a5ed1b0cdd
Paul Hoffman <phoffman@nygenome.org>
2018-11-19 17:03:32 -0800

    Adjust code style, update parameters to be consistent with Seurat, add author information

--- R/utilities.R
+++ R/utilities.R
@@ -509,6 +509,191 @@ ExpMean <- function(x) {
   return(log(x = mean(x = exp(x = x) - 1) + 1))
 }
 
+#' Export Seurat object for UCSC cell browser
+#'
+#' @param object Seurat object
+#' @param dir output directory path
+#' @param dataset.name name of the dataset
+#' @param vars vector of metadata fields to export
+#' @param vars.names list that defines metadata field names after the export.
+#' Should map metadata column name to export name
+#' @param reductions vector of reduction names to export
+#' @param markers.file path to file with marker genes
+#' @param cluster.field name of the metadata field containing cell cluster
+#' @param cb.dir in which dir to create UCSC cell browser content root
+#' @param port \emph{experimental} on which port to run UCSC cell browser after export
+#'
+#' @return ...
+#'
+#' @author Maximilian Haeussler, Nikolay Markov
+#'
+#' @importFrom utils browseURL
+#' @importFrom reticulate py_module_available import
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ExportToCellbrowser(object = pbmc_small, dataset.name = "PBMC", dir = "out")
+#' }
+#'
+ExportToCellbrowser <- function(
+  object,
+  dir,
+  dataset.name,
+  vars = NULL,
+  vars.names = NULL,
+  reductions = "tsne",
+  markers.file = NULL,
+  cluster.field = NULL,
+  port = NULL,
+  cb.dir = NULL
+) {
+  if (is.null(x = vars)) {
+    vars <- c("nCount_RNA", "nFeature_RNA")
+    if (length(x = levels(x = Idents(object = object))) > 1) {
+      vars <- c(vars, ".ident")
+    }
+  }
+  if (!is.null(x = port) && is.null(x = cb.dir)) {
+    stop("cb.dir parameter is needed when port is set")
+  }
+  if (!dir.exists(paths = dir)) {
+    dir.create(path = dir)
+  }
+  if (!dir.exists(paths = dir)) {
+    stop("Output directory ", dir, " cannot be created or is a file")
+  }
+  order <- colnames(x = object)
+  enum.fields <- c()
+  # Export expression matrix
+  df <- as.data.frame(x = as.matrix(x = GetAssayData(object = object)))
+  df <- data.frame(gene = rownames(x = object), df)
+  z <- gzfile(file.path(dir, "exprMatrix.tsv.gz"), "w")
+  write.table(x = df, sep = "\t", file = z, quote = FALSE, row.names = FALSE)
+  close(con = z)
+  # Export cell embeddings
+  embeddings.conf <- c()
+  for (reduction in reductions) {
+    df <- Embeddings(object = object, reduction = reduction)
+    if (ncol(x = df) > 2) {
+      warning(
+        'Embedding ',
+        reduction,
+        ' has more than 2 coordinates, taking only the first 2'
+      )
+      df <- df[, 1:2]
+    }
+    colnames(x = df) <- c("x", "y")
+    df <- data.frame(cellId = rownames(x = df), df)
+    fname <- file.path(dir, paste0(reduction, '.coords.tsv'))
+    write.table(
+      x = df[order, ],
+      sep = "\t",
+      file = fname,
+      quote = FALSE,
+      row.names = FALSE
+    )
+    conf <- sprintf(
+      '{"file": "%s.coords.tsv", "shortLabel": "Seurat %1$s"}',
+      reduction
+    )
+    embeddings.conf <- c(embeddings.conf, conf)
+  }
+  # Export metadata
+  df <- data.frame(row.names = rownames(x = object[[]]))
+  for (field in vars) {
+    if (field == ".ident") {
+      df$Cluster <- Idents(object = object)
+      enum.fields <- c(enum.fields, "Cluster")
+      cluster.field <- "Cluster"
+    } else {
+      name <- vars.names[[field]]
+      name <- name %||% field
+      df[[name]] <- object[[field]][, 1]
+      if (!is.numeric(x = df[[name]])) {
+        enum.fields <- c(enum.fields, name)
+      }
+    }
+  }
+  df <- data.frame(Cell = rownames(x = df), df)
+  write.table(
+    x = df[order, ],
+    sep = "\t",
+    file = file.path(dir, "meta.tsv"),
+    quote = FALSE,
+    row.names = FALSE
+  )
+  # Export markers
+  markers.string <- ''
+  if (!is.null(x = markers.file)) {
+    file.copy(from = markers.file, to = file.path(dir, "markers.tsv"))
+    markers.string <- 'markers = [{"file": "markers.tsv", "shortLabel": "Seurat Cluster Markers"}]'
+  }
+  config <- c(
+    'name="%s"',
+    'shortLabel="%1$s"',
+    'exprMatrix="exprMatrix.tsv.gz"',
+    '#tags = ["10x", "smartseq2"]',
+    'meta="meta.tsv"',
+    '# possible values: "gencode-human", "gencode-mouse", "symbol" or "auto"',
+    'geneIdType="auto"',
+    'clusterField="%s"',
+    'labelField="%2$s"',
+    'enumFields=%s',
+    '%s',
+    'coords=%s'
+  )
+  config <- paste(config, collapse = '\n')
+  enum.string <- paste0(
+    "[",
+    paste(paste0('"', enum.fields, '"'), collapse = ", "),
+    "]"
+  )
+  coords.string <- paste0(
+    "[",
+    paste(embeddings.conf, collapse = ",\n"),
+    "]"
+  )
+  config <- sprintf(
+    config,
+    dataset.name,
+    cluster.field,
+    enum.string,
+    markers.string,
+    coords.string
+  )
+  cat(config, file = file.path(dir, "cellbrowser.conf"))
+  message("Prepared cellbrowser directory ", dir)
+  if (!is.null(x = cb.dir)) {
+    if (!py_module_available(module = "cellbrowser")) {
+      stop(
+        "The Python package `cellbrowser` is required to prepare and run ",
+        "Cellbrowser. Please install it ",
+        "on the Unix command line with `sudo pip install cellbrowser` (if root) ",
+        "or `pip install cellbrowser --user` (as a non-root user). ",
+        "To adapt the Python that is used, you can either set the env. variable RETICULATE_PYTHON ",
+        "or do `require(reticulate) and use one of these functions: use_python(), use_virtualenv(), use_condaenv(). ",
+        "See https://rstudio.github.io/reticulate/articles/versions.html; ",
+        "at the moment, R's reticulate is using this Python: ",
+        import(module = 'sys')$executable,
+        ". "
+      )
+    }
+    if (!is.null(x = port)) {
+      port <- as.integer(x = port)
+    }
+    cb <- import(module = "cellbrowser")
+    cb$cellbrowser$build(dir, cb.dir)
+    if (!is.null(port)) {
+      cb$cellbrowser$stop()
+      cb$cellbrowser$serve(cb.dir, port)
+      Sys.sleep(time = 0.4)
+      browseURL(url = paste0("http://localhost:", port))
+    }
+  }
+}
+
 #' Calculate the standard deviation of logged values
 #'
 #' Calculate SD of logged values in non-log space (return answer in log-space)
@@ -635,6 +820,27 @@ MinMax <- function(data, min, max) {
   data2[data2 > max] <- max
   data2[data2 < min] <- min
   return(data2)
+}
+
+#' Stop Cellbrowser web server
+#'
+#' @importFrom reticulate py_module_available
+#' @importFrom reticulate import
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' StopCellbrowser()
+#' }
+#'
+StopCellbrowser <- function() {
+  if (py_module_available(module = "cellbrowser")) {
+    cb <- import(module = "cellbrowser")
+    cb$cellbrowser$stop()
+  } else {
+    stop("The `cellbrowser` package is not available in the Python used by R's reticulate")
+  }
 }
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -1147,202 +1353,4 @@ ToNumeric <- function(x){
     return(num)
   }
   return(x)
-}
-
-#' Export Seurat object for UCSC cell browser
-#'
-#' @param object Seurat object
-#' @param dir output directory path
-#' @param dataset.name name of the dataset
-#' @param meta.fields vector of metadata fields to export
-#' @param meta.fields.names list that defines metadata field names
-#'                          after the export. Should map metadata
-#'                          column name to export name
-#' @param embeddings vector of embedding names to export
-#' @param markers.file path to file with marker genes
-#' @param cluster.field name of the metadata field containing cell cluster
-#' @param cb.dir in which dir to create UCSC cell browser content root
-#' @param port \emph{experimental} on which port to run UCSC cell browser after export
-#'
-#' @export
-#'
-#' @importFrom reticulate py_module_available
-#' @importFrom reticulate import
-#'
-#' @examples
-#' \dontrun{
-#' ExportToCellbrowser(object = pbmc_small, dataset.name = "PBMC", dir = "out")
-#' }
-#'
-ExportToCellbrowser <- function(
-  object,
-  dir,
-  dataset.name,
-  meta.fields = NULL,
-  meta.fields.names = NULL,
-  embeddings = c("tsne"),
-  markers.file = NULL,
-  cluster.field = NULL,
-  port = NULL,
-  cb.dir = NULL
-) {
-  if (is.null(meta.fields)) {
-    meta.fields <- c("nCount_RNA", "nFeature_RNA")
-    if (length(levels(x = Idents(object = object))) > 1) {
-      meta.fields <- c(meta.fields, ".ident")
-    }
-  }
-  if (!is.null(port) && is.null(cb.dir)) {
-    stop("cb.dir parameter is needed when port is set")
-  }
-  if (!dir.exists(dir)) {
-    dir.create(dir)
-  }
-  if (!dir.exists(dir)) {
-    stop("Output directory ", dir, " cannot be created or is a file")
-  }
-
-  order <- Cells(object = object)
-  enum.fields <- c()
-
-  # Export expression matrix
-  df <- as.data.frame(as.matrix(GetAssayData(object = object)))
-  df <- data.frame(gene=rownames(object), df)
-  z <- gzfile(file.path(dir, "exprMatrix.tsv.gz"), "w")
-  write.table(df, sep="\t", file=z, quote = FALSE, row.names = FALSE)
-  close(z)
-
-  # Export cell embeddings
-  embeddings.conf <- c()
-  for (embedding in embeddings) {
-    df <- Embeddings(object = object, reduction = embedding)
-    if (ncol(df) > 2) {
-      warning(
-        'Embedding ', embedding,
-        ' has more than 2 coordinates, taking only the first 2',
-      )
-      df <- df[, 1:2]
-    }
-    colnames(df) <- c("x", "y")
-    df <- data.frame(cellId = rownames(df), df)
-    fname <- file.path(
-      dir,
-      sprintf("%s.coords.tsv", embedding)
-    )
-    write.table(df[order, ], sep="\t", file=fname, quote = FALSE, row.names = FALSE)
-    conf <- sprintf(
-      '{"file": "%s.coords.tsv", "shortLabel": "Seurat %1$s"}',
-      embedding
-    )
-    embeddings.conf <- c(embeddings.conf, conf)
-  }
-
-  # Export metadata
-  df <- data.frame(row.names = rownames(object[[]]))
-  for (field in meta.fields) {
-    if (field == ".ident") {
-      df$Cluster <- Idents(object = object)
-      enum.fields <- c(enum.fields, "Cluster")
-      cluster.field <- "Cluster"
-    } else {
-      name <- meta.fields.names[[field]]
-      if (is.null(name)) {
-        name <- field
-      }
-      df[[name]] <- object[[field]][, 1]
-      if (!is.numeric(df[[name]])) {
-        enum.fields <- c(enum.fields, name)
-      }
-    }
-  }
-  df <- data.frame(Cell=rownames(df), df)
-  write.table(df[order, ], sep="\t", file=file.path(dir, "meta.tsv"), quote = FALSE, row.names=FALSE)
-
-  # Export markers
-  markers.string <- ''
-  if (!is.null(markers.file)) {
-    file.copy(markers.file, file.path(dir, "markers.tsv"))
-    markers.string <- 'markers = [{"file": "markers.tsv", "shortLabel": "Seurat Cluster Markers"}]'
-  }
-
-  config <- 'name="%s"
-shortLabel="%1$s"
-exprMatrix="exprMatrix.tsv.gz"
-#tags = ["10x", "smartseq2"]
-meta="meta.tsv"
-# possible values: "gencode-human", "gencode-mouse", "symbol" or "auto"
-geneIdType="auto"
-clusterField="%s"
-labelField="%2$s"
-enumFields=%s
-%s
-coords=%s'
-  enum.string <- paste0(
-    "[",
-    paste(paste0('"', enum.fields, '"'), collapse = ", "),
-    "]"
-  )
-  coords.string <- paste0(
-    "[",
-    paste(embeddings.conf, collapse = ",\n"),
-    "]"
-  )
-  config <- sprintf(
-    config,
-    dataset.name,
-    cluster.field,
-    enum.string,
-    markers.string,
-    coords.string
-  )
-  cat(config, file=file.path(dir, "cellbrowser.conf"))
-  message("Prepared cellbrowser directory ", dir)
-
-  if (!is.null(cb.dir)) {
-    if (!py_module_available("cellbrowser")) {
-          stop(
-            "The Python package `cellbrowser` is required to prepare and run ",
-            "Cellbrowser. Please install it ",
-            "on the Unix command line with `sudo pip install cellbrowser` (if root) ",
-            "or `pip install cellbrowser --user` (as a non-root user). ",
-            "To adapt the Python that is used, you can either set the env. variable RETICULATE_PYTHON ",
-            "or do `require(reticulate) and use one of these functions: use_python(), use_virtualenv(), use_condaenv(). ",
-            "See https://rstudio.github.io/reticulate/articles/versions.html . ",
-            "At the moment, R's reticulate is using this Python: ",import('sys')$executable,". "
-           )
-    }
-
-    if (!is.null(port)) {
-      port <- as.integer(port)
-    }
-    cb <- import("cellbrowser")
-    cb$cellbrowser$build(dir, cb.dir)
-    if (!is.null(port)) {
-      cb$cellbrowser$stop()
-      cb$cellbrowser$serve(cb.dir, port)
-      Sys.sleep(0.4)
-      utils::browseURL(sprintf("http://localhost:%d", port))
-    }
-  }
-}
-
-#' Stop Cellbrowser web server
-#'
-#' @export
-#'
-#' @importFrom reticulate py_module_available
-#' @importFrom reticulate import
-#'
-#' @examples
-#' \dontrun{
-#' StopCellbrowser()
-#' }
-#'
-StopCellbrowser <- function() {
-  if (py_module_available("cellbrowser")) {
-    cb <- import("cellbrowser")
-    cb$cellbrowser$stop()
-  } else {
-    stop("The `cellbrowser` package is not available in the Python used by R's reticulate")
-  }
 }
