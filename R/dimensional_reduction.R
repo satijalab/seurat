aeb4897243fb0f6689a51464e1ed67ca6528f52a
Scott Gigante <scottgigante@gmail.com>
2019-02-21 17:23:15 -0500

    add RunPHATE for Seurat 3

--- R/dimensional_reduction.R
+++ R/dimensional_reduction.R
@@ -1385,6 +1385,239 @@ RunUMAP.Seurat <- function(
   return(object)
 }
 
+
+
+#' @importFrom phateR phate
+#'
+#' @rdname RunPHATE
+#' @method RunPHATE default
+#' @export
+#'
+RunPHATE.default <- function(
+  object,
+  dims = NULL,
+  reduction = 'pca',
+  features = NULL,
+  assay = 'RNA',
+  n.components = 2L,
+  k = 15L,
+  alpha = 10L,
+  n.landmark=2000L,
+  potential.method = "log",
+  t = "auto",
+  knn.dist.method = "euclidean",
+  mds.method = "metric",
+  mds.dist.method = "euclidean",
+  t.max=100,
+  npca = 100,
+  plot.optimal.t = FALSE,
+  verbose = 1,
+  n.jobs = 1,
+  seed.use = NA,
+  reduction.key = "PHATE_",
+  ...
+) {
+  data.use <- t(data.use)
+  phate_output <- phate(
+    data.use,
+    ndim=n.components,
+    k = k,
+    alpha = alpha,
+    n.landmark = n.landmark,
+    potential.method = potential.method,
+    t = t,
+    knn.dist.method = knn.dist.method,
+    init = NULL,
+    mds.method = mds.method,
+    mds.dist.method = mds.dist.method,
+    t.max = t.max,
+    npca = npca,
+    plot.optimal.t = plot.optimal.t,
+    verbose = verbose,
+    n.jobs = n.jobs,
+    seed = seed.use,
+    ...
+  )
+  phate_output <- as.matrix(phate_output)
+  colnames(x = phate_output) <- paste0(reduction.key, 1:ncol(x = phate_output))
+  rownames(x = phate_output) <- rownames(object)
+  phate.reduction <- CreateDimReducObject(
+    embeddings = phate_output,
+    key = reduction.key,
+    assay = assay
+  )
+  return(phate.reduction)
+}
+
+#' Run PHATE
+#'
+#' PHATE is a data reduction method specifically designed for visualizing
+#' **high** dimensional data in **low** dimensional spaces.
+#' To run, you must first install the `phate` python
+#' package (e.g. via pip install phate). Details on this package can be
+#' found here: \url{https://github.com/KrishnaswamyLab/PHATE}. For a more in depth
+#' discussion of the mathematics underlying PHATE, see the bioRxiv paper here:
+#' \url{https://www.biorxiv.org/content/early/2017/12/01/120378}.
+#'
+#' @param dims Which dimensions to use as input features, used only if
+#' \code{features} is NULL
+#' @param reduction Which dimensional reduction (PCA or ICA) to use for the
+#' UMAP input. Default is PCA
+#' @param features If set, run UMAP on this subset of features (instead of running on a
+#' set of reduced dimensions). Not set (NULL) by default; \code{dims} must be NULL to run
+#' on features
+#' @param assay Assay to pull data for when using \code{features}, or assay used to construct Graph
+#' if running UMAP on a Graph
+#' @param max.dim Total number of dimensions to embed in PHATE.
+#' @param k int, optional, default: 15
+#' number of nearest neighbors on which to build kernel
+#' @param alpha int, optional, default: 10
+#' sets decay rate of kernel tails.
+#' If NA, alpha decaying kernel is not used
+#' @param n.landmark int, optional, default: 2000
+#' number of landmarks to use in fast PHATE
+#' @param gamma float, optional, default: 1
+#' Informational distance constant between -1 and 1.
+#' `gamma=1` gives the PHATE log potential, `gamma=0` gives
+#' a square root potential.
+#' @param t int, optional, default: 'auto'
+#' power to which the diffusion operator is powered
+#' sets the level of diffusion
+#' @param knn.dist.method string, optional, default: 'euclidean'.
+#' recommended values: 'euclidean', 'cosine', 'precomputed'
+#' Any metric from `scipy.spatial.distance` can be used
+#' distance metric for building kNN graph. If 'precomputed',
+#' `data` should be an n_samples x n_samples distance or
+#' affinity matrix. Distance matrices are assumed to have zeros
+#' down the diagonal, while affinity matrices are assumed to have
+#' non-zero values down the diagonal. This is detected automatically using
+#' `data[0,0]`. You can override this detection with
+#' `knn.dist.method='precomputed_distance'` or
+#' `knn.dist.method='precomputed_affinity'`.
+#' @param mds.method string, optional, default: 'metric'
+#' choose from 'classic', 'metric', and 'nonmetric'
+#' which MDS algorithm is used for dimensionality reduction
+#' @param mds.dist.method string, optional, default: 'euclidean'
+#' recommended values: 'euclidean' and 'cosine'
+#' @param t.max int, optional, default: 100.
+#' Maximum value of t to test for automatic t selection.
+#' @param npca int, optional, default: 100
+#' Number of principal components to use for calculating
+#' neighborhoods. For extremely large datasets, using
+#' n_pca < 20 allows neighborhoods to be calculated in
+#' log(n_samples) time.
+#' @param plot.optimal.t boolean, optional, default: FALSE
+#' If TRUE, produce a plot showing the Von Neumann Entropy
+#' curve for automatic t selection.
+#' @param verbose `int` or `boolean`, optional (default : 1)
+#' If `TRUE` or `> 0`, print verbose updates.
+#' @param n.jobs `int`, optional (default: 1)
+#' The number of jobs to use for the computation.
+#' If -1 all CPUs are used. If 1 is given, no parallel computing code is
+#' used at all, which is useful for debugging.
+#' For n_jobs below -1, (n.cpus + 1 + n.jobs) are used. Thus for
+#' n_jobs = -2, all CPUs but one are used
+#' @param seed.use int or `NA`, random state (default: `NA`)
+#' @param reduction.name dimensional reduction name, specifies the position in
+#' the object$dr list. phate by default
+#' @param reduction.key dimensional reduction key, specifies the string before
+#' the number for the dimension names. PHATE_ by default
+#' @param ... Additional arguments for `phateR::phate`
+#'
+#' @return Returns a Seurat object containing a PHATE representation
+#'
+#' @references Moon K, van Dijk D, Wang Z, Burkhardt D, Chen W, van den Elzen A,
+#' Hirn M, Coifman R, Ivanova N, Wolf G and Krishnaswamy S (2017).
+#' "Visualizing Transitions and Structure for High Dimensional Data
+#' Exploration." _bioRxiv_, pp. 120378. doi: 10.1101/120378
+#' (URL: http://doi.org/10.1101/120378),
+#' <URL: https://www.biorxiv.org/content/early/2017/12/01/120378>.
+#' @examples
+#' if (reticulate::py_module_available("phate")) {
+#'
+#' # Load data
+#' pbmc_small
+#'
+#' # Run PHATE with default parameters
+#' pbmc_small <- RunPHATE(object = pbmc_small)
+#' # Plot results
+#' DimPlot(object = pbmc_small, reduction.use = 'phate')
+#'
+#' # Try smaller `k` for a small dataset, and larger `t` for a noisy embedding
+#' pbmc_small <- RunPHATE(object = pbmc_small, k = 4, t = 12)
+#' # Plot results
+#' DimPlot(object = pbmc_small, reduction.use = 'phate')
+#'
+#' # For increased emphasis on local structure, use sqrt potential
+#' pbmc_small <- RunPHATE(object = pbmc_small, potential.method='sqrt')
+#' # Plot results
+#' DimPlot(object = pbmc_small, reduction.use = 'phate')
+#'
+#' }
+#' @export
+#'
+RunPHATE.Seurat <- function(
+  object,
+  dims = NULL,
+  reduction = 'pca',
+  features = NULL,
+  assay = 'RNA',
+  n.components = 2L,
+  k = 15L,
+  alpha = 10L,
+  n.landmark=2000L,
+  potential.method = "log",
+  t = "auto",
+  knn.dist.method = "euclidean",
+  mds.method = "metric",
+  mds.dist.method = "euclidean",
+  t.max = 100,
+  npca = 100,
+  plot.optimal.t = FALSE,
+  verbose = 1,
+  n.jobs = 1,
+  seed.use = NA,
+  reduction.name = "phate",
+  reduction.key = "PHATE_",
+  ...
+) {
+  if (sum(c(is.null(x = dims), is.null(x = features)) > 1) {
+      stop("Please specify only one of the following arguments: dims or features")
+  }
+  if (!is.null(x = features)) {
+    data.use <- t(x = GetAssayData(object = object, slot = 'data', assay = assay)[features, ])
+  } else if (!is.null(x = dims)) {
+    data.use <- Embeddings(object[[reduction]])[, dims]
+    assay <- DefaultAssay(object = object[[reduction]])
+  } else {
+    stop("Please specify one of dims or features")
+  }
+  object[[reduction.name]] <- RunPHATE(
+    object = data.use,
+    assay = assay,
+    n.components = n.components,
+    k = k,
+    alpha = alpha,
+    n.landmark = n.landmark,
+    potential.method = potential.method,
+    t = t,
+    knn.dist.method = knn.dist.method,
+    mds.method = mds.method,
+    mds.dist.method = mds.dist.method,
+    t.max = t.max,
+    npca = npca,
+    plot.optimal.t = plot.optimal.t,
+    verbose = verbose,
+    n.jobs = n.jobs,
+    seed.use = seed.use,
+    reduction.key = reduction.key,
+    ...
+  )
+  object <- LogSeuratCommand(object = object)
+  return(object)
+}
+
+
 #' @param dims Which dimensions to examine
 #' @param score.thresh Threshold to use for the proportion test of PC
 #' significance (see Details)
