e4108b7c7af22ae5d71cc848b4d5e51256b22999
Paul Hoffman <mojaveazure@users.noreply.github.com>
2019-04-04 13:52:11 -0400

    Update objects.R

--- R/objects.R
+++ R/objects.R
@@ -2607,11 +2607,11 @@ ReadH5AD.H5File <- function(file, assay = 'RNA', verbose = TRUE, ...) {
   if (verbose) {
     message("Pulling expression matrices and metadata")
   }
-  x <- if (is(object = file[['X']], class2 = 'H5Group')) {
-    as.sparse(x = file[['X']])
-    x@Dim <- c(length(file[["var"]][]$index), length(file[["obs"]][]$index))
+  if (is(object = file[['X']], class2 = 'H5Group')) {
+    x <- as.sparse(x = file[['X']])
+    slot(object = x, name = 'Dim') <- c(length(file[["var"]][]$index), length(file[["obs"]][]$index))
   } else {
-    file[['X']][, ]
+    x <- file[['X']][, ]
   }
   # x will be an S3 matrix if X was scaled, otherwise will be a dgCMatrix
   scaled <- is.matrix(x = x)
@@ -2626,6 +2626,7 @@ ReadH5AD.H5File <- function(file, assay = 'RNA', verbose = TRUE, ...) {
   # Pull raw expression matrix and feature-level metadata
   if (file$exists(name = 'raw.X')) {
     raw <- as.sparse(x = file[['raw.X']])
+    slot(object = raw, name = 'Dim') <- c(length(file[["var"]][]$index), length(file[["obs"]][]$index))
     raw.var <- file[['raw.var']][]
     rownames(x = raw) <- rownames(x = raw.var) <- raw.var$index
     colnames(x = raw) <- obs$index
