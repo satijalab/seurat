db6e286099fc30b6187d6c45e33e96a6c313bfd0
Sergio Oller <sergioller@gmail.com>
2019-04-08 15:48:22 +0200

    Let RunPCA report total variance

--- tests/testthat/test_dimensional_reduction.R
+++ tests/testthat/test_dimensional_reduction.R
@@ -21,3 +21,28 @@ test_that("different ways of passing distance matrix", {
   expect_equivalent(RunTSNE(obj, distance.matrix = distMat)@reductions$tsne,
                     RunTSNE(as.matrix(distMat), assay = "RNA", is_distance = TRUE))
 })
+
+test_that("pca returns total variance (see #982)", {
+  # Generate dummy data exp matrix
+  set.seed(1)
+  dummyexpMat <- matrix(data = sample(x = c(1:50), size = 1e4, replace = TRUE),
+                        ncol = 100, nrow = 100)
+  colnames(dummyexpMat) <- paste0("cell", seq(ncol(dummyexpMat)))
+  row.names(dummyexpMat) <- paste0("gene", seq(nrow(dummyexpMat)))
+
+  # Create Seurat object for testing
+  obj <- CreateSeuratObject(counts = dummyexpMat)
+
+  # Scale and compute PCA, using RunPCA
+  obj <- ScaleData(obj)
+  pca_result <- RunPCA(obj, features = rownames(obj))
+
+  # Using stats::prcomp
+  scaled_data <- Seurat::GetAssayData(object = obj, slot = "scale.data")
+  prcomp_result <- stats::prcomp(scaled_data, center = FALSE, scale. = FALSE)
+
+  # Compare
+  expect_equivalent(slot(pca_result[["pca"]], "misc")$total_variance,
+                    sum(prcomp_result$sdev^2))
+
+})
