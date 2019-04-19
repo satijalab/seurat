27359e6a41344feb79f909bab70c66f148b10da0
Andrew Butler <abutler@nygenome.org>
2019-04-19 14:53:38 -0400

    fall back on cbind for merging matrices if rownames match exactly

--- R/utilities.R
+++ R/utilities.R
@@ -1499,19 +1499,23 @@ RowMergeSparseMatrices <- function(mat1, mat2){
   if (inherits(x = mat2, what = "data.frame")) {
     mat2 <- as.matrix(x = mat2)
   }
-  mat1 <- as(object = mat1, Class = "RsparseMatrix")
-  mat2 <- as(object = mat2, Class = "RsparseMatrix")
   mat1.names <- rownames(x = mat1)
   mat2.names <- rownames(x = mat2)
-  all.names <- union(x = mat1.names, y = mat2.names)
-  new.mat <- RowMergeMatrices(
-    mat1 = mat1,
-    mat2 = mat2,
-    mat1_rownames = mat1.names,
-    mat2_rownames = mat2.names,
-    all_rownames = all.names
-  )
-  rownames(x = new.mat) <- make.unique(names = all.names)
+  if (all(mat1.names == mat2.names)) {
+    new.mat <- cbind(mat1, mat2)
+  } else {
+    mat1 <- as(object = mat1, Class = "RsparseMatrix")
+    mat2 <- as(object = mat2, Class = "RsparseMatrix")
+    all.names <- union(x = mat1.names, y = mat2.names)
+    new.mat <- RowMergeMatrices(
+      mat1 = mat1,
+      mat2 = mat2,
+      mat1_rownames = mat1.names,
+      mat2_rownames = mat2.names,
+      all_rownames = all.names
+    )
+    rownames(x = new.mat) <- make.unique(names = all.names)
+  }
   colnames(x = new.mat) <- make.unique(names = c(
     colnames(x = mat1),
     colnames(x = mat2)
