cd4062b84f90922b1eb9b7c5cc1cdfb94d852364
Andrew Butler <abutler@nygenome.org>
2019-05-20 15:56:41 -0400

    bug fix for DoHeatmap when using group.by with draw.lines=TRUE

--- R/visualization.R
+++ R/visualization.R
@@ -264,12 +264,13 @@ DoHeatmap <- function(
   # group.use <- factor(x = group.use[cells])
   plots <- vector(mode = 'list', length = ncol(x = groups.use))
   for (i in 1:ncol(x = groups.use)) {
+    data.group <- data
     group.use <- groups.use[, i, drop = TRUE]
     group.use <- factor(x = group.use)
     names(x = group.use) <- cells
     if (draw.lines) {
       # create fake cells to serve as the white lines, fill with NAs
-      lines.width <- lines.width %||% ceiling(x = nrow(x = data) * 0.0025)
+      lines.width <- lines.width %||% ceiling(x = nrow(x = data.group) * 0.0025)
       placeholder.cells <- sapply(
         X = 1:(length(x = levels(x = group.use)) * lines.width),
         FUN = function(x) RandomName(length = 20)
@@ -279,11 +280,11 @@ DoHeatmap <- function(
       group.use <- as.vector(x = group.use)
       names(x = group.use) <- cells
       group.use <- factor(x = c(group.use, placeholder.groups))
-      na.data <- matrix(data = NA, nrow = length(x = placeholder.cells), ncol = ncol(data), dimnames = list(placeholder.cells, colnames(x = data)))
-      data <- rbind(data, na.data)
+      na.data.group <- matrix(data = NA, nrow = length(x = placeholder.cells), ncol = ncol(data.group), dimnames = list(placeholder.cells, colnames(x = data.group)))
+      data.group <- rbind(data.group, na.data.group)
     }
     plot <- SingleRasterMap(
-      data = data,
+      data = data.group,
       raster = raster,
       disp.min = disp.min,
       disp.max = disp.max,
