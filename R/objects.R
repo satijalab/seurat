8dfc31c974fcbe08d4121e026320d0d13ba43488
timoast <4591688+timoast@users.noreply.github.com>
2019-08-09 10:32:15 -0400

    add else if

--- R/objects.R
+++ R/objects.R
@@ -699,7 +699,7 @@ CreateSeuratObject <- function(
   if (!is.null(x = meta.data)) {
     if (is.null(x = rownames(x = meta.data))) {
       stop("Row names not set in metadata. Please ensure that rownames of metadata match column names of data matrix")
-    } if (length(x = setdiff(x = rownames(x = meta.data), y = colnames(x = counts)))) {
+    } else if (length(x = setdiff(x = rownames(x = meta.data), y = colnames(x = counts)))) {
       stop("Not all cells present in both metadata and counts")
     }
   }
