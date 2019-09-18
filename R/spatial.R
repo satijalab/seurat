3bdea93f0ceec467eb097432799d237d56fbfef6
Paul Hoffman <mojaveazure@user.noreply.github.com>
2019-09-18 15:22:22 -0400

    Minor stylistic changes

--- R/spatial.R
+++ R/spatial.R
@@ -27,56 +27,67 @@ SpatialAssay <- setClass(
 # Functions
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Read10xSpatial <- function(outs_path, filtered_feature_bc_matrix_path = outs_path, image_path=outs_path, scale_factors_path=outs_path, tissue_positions_path=outs_path, filter_matrix=TRUE, assay = "Spatial") {
-  
-  if(missing(filtered_feature_bc_matrix_path)) {
-    a <- Seurat::Read10X_h5(paste(outs_path, "/filtered_feature_bc_matrix.h5", sep = ""))
+Read10xSpatial <- function(
+  outs_path,
+  filtered_feature_bc_matrix_path = outs_path,
+  image_path = outs_path,
+  scale_factors_path = outs_path,
+  tissue_positions_path = outs_path,
+  filter_matrix = TRUE,
+  assay = "Spatial"
+) {
+  a <- if (missing(x = filtered_feature_bc_matrix_path)) {
+    Read10X_h5(filename = file.path(outs_path, "filtered_feature_bc_matrix.h5"))
   } else {
-    a <- Seurat::Read10X_h5(filtered_feature_bc_matrix_path)
+    Read10X_h5(filename = filtered_feature_bc_matrix_path)
   }
-    
-  if(missing(image_path)) {
-   image <- png::readPNG(paste(outs_path, "/spatial/tissue_lowres_image.jpg", sep = ""))
+  if (missing(x = image_path)) {
+   image <- png::readPNG(source = file.path(outs_path, "spatial", "tissue_lowres_image.jpg"))
   } else {
-   image <- png::readPNG(image_path)
-    }
-    height <- nrow(image)
-    width <- ncol(image)
-    image <- list(grid::rasterGrob(image, width=unit(1,"npc"), height=unit(1,"npc")))
-    image <- tibble(grob=image)
-  
-  if(missing(scale_factors_path)) {
-    scale.factors <- jsonlite::fromJSON(txt = paste(outs_path, "/spatial/scalefactors_json.json", sep = ""))
+   image <- png::readPNG(source = image_path)
+  }
+  height <- nrow(x = image)
+  width <- ncol(x = image)
+  image <- list(grid::rasterGrob(
+    image = image,
+    width = unit(x = 1, units = "npc"),
+    height = unit(x = 1, units = "npc")
+  ))
+  image <- tibble(grob = image)
+  if (missing(x = scale_factors_path)) {
+    scale.factors <- jsonlite::fromJSON(txt = file.path(outs_path, "spatial", "scalefactors_json.json"))
   } else {
     scale.factors <- jsonlite::fromJSON(txt = scale_factors_path)
   }
-    
-  if(missing(tissue_positions_path)) {  
-    tissue.positions <- read.csv(paste(outs_path,"/spatial/tissue_positions_list.txt", sep = ""),col.names=c("barcode","tissue","row","col","imagerow","imagecol"), header = FALSE)
+  if (missing(x = tissue_positions_path)) {
+    tissue.positions <- read.csv(
+      file = file.path(outs_path, "spatial", "tissue_positions_list.txt"),
+      col.names = c("barcode", "tissue", "row", "col", "imagerow", "imagecol"),
+      header = FALSE
+    )
   } else {
-    tissue.positions <- read.csv(tissue_positions_path,col.names=c("barcode","tissue","row","col","imagerow","imagecol"), header = FALSE)
+    tissue.positions <- read.csv(
+      file = tissue_positions_path,
+      col.names = c("barcode", "tissue", "row", "col", "imagerow", "imagecol"),
+      header = FALSE
+    )
   }
-    
-    tissue.positions$scaled_imagerow <- tissue.positions$imagerow * scale.factors$tissue_lowres_scalef
-    tissue.positions$scaled_imagecol <- tissue.positions$imagecol * scale.factors$tissue_lowres_scalef
-    
-   image$image_height <- height
-   image$image_width <- width
-    
-    # Filter for only spots under tissue if TRUE (default)
-   if(filter_matrix) {
-    tissue.positions <- tissue.positions %>%
-      filter(tissue == 1)
-   }
-  
-   
-  CreateAssayObject(counts = a, image = image, scale.factors = scale.factors, tissue.positions = tissue.positions)
-  
-  assays <- list(CreateAssayObject(counts = a, image = image, scale.factors = scale.factors, tissue.positions = tissue.positions))
+  tissue.positions$scaled_imagerow <- tissue.positions$imagerow * scale.factors$tissue_lowres_scalef
+  tissue.positions$scaled_imagecol <- tissue.positions$imagecol * scale.factors$tissue_lowres_scalef
+  image$image_height <- height
+  image$image_width <- width
+  # Filter for only spots under tissue if TRUE (default)
+  if (filter_matrix) {
+    tissue.positions <- tissue.positions %>% filter(tissue == 1)
+  }
+  assays <- list(CreateAssayObject(
+    counts = a,
+    image = image,
+    scale.factors = scale.factors,
+    tissue.positions = tissue.positions
+  ))
   names(x = assays) <- assay
-  
   Key(object = assays[[assay]]) <- suppressWarnings(expr = UpdateKey(key = tolower(x = assay)))
-  
   object <- new(
     Class = "Seurat",
     assays = assays,
@@ -85,7 +96,6 @@ Read10xSpatial <- function(outs_path, filtered_feature_bc_matrix_path = outs_pat
     project.name = "SeuratProject"
   )
  DefaultAssay(object = object) <- assay
- 
  return(object)
 }
 
