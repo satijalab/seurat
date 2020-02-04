9ea819b2d32cccf522ddd3b4365122a46a9d31dc
Paul Hoffman <mojaveazure@users.noreply.github.com>
2020-02-04 17:38:33 -0500

    Style fixes

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1175,8 +1175,7 @@ SampleUMI <- function(
 #' @importFrom stats setNames
 #' @importFrom sctransform vst get_residual_var get_residuals correct_counts
 #'
-#' @seealso \code{\link[sctransform:correct_counts]{sctransform::correct_counts}}
-#' @seealso \code{\link[sctransform:get_residuals]{sctransform::get_residuals}}
+#' @seealso \code{\link[sctransform]{correct_counts}} \code{\link[sctransform]{get_residuals}}
 #' @export
 #'
 #' @examples
