099d745a76f6771617d3bc31c6ac31197c3f4374
yuhanH <yh1970@nyu.edu>
2020-05-04 16:38:04 -0400

    fix getresidual bug when one vst.set empty

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -539,6 +539,7 @@ GetResidual <- function(
       vst_out <- vst.set[[v]]
       # confirm that cells from SCT model also exist in the integrated object
       cells.v <- intersect(x = rownames(x = vst_out$cell_attr), y = Cells(x = object))
+      if(length(cells.v) !=0){
       vst_out$cell_attr <- vst_out$cell_attr[cells.v, ]
       vst_out$cells_step1 <- intersect(x = vst_out$cells_step1, y = cells.v)
       object.v <- subset(x = object, cells = cells.v)
@@ -556,7 +557,9 @@ GetResidual <- function(
         new.scale.data,
         GetAssayData(object = object.v, assay = assay, slot ="scale.data" )[new_features, , drop = FALSE]
       )
+      }
     }
+
     object <- SetAssayData(
       object = object,
       assay = assay,
