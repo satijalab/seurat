3d4f075abea574f838d741b1c14663900a9799f3
Yuhan Hao <haoy@satijalab05.nygenome.org>
2020-06-30 12:53:45 -0400

    add prefix to feature 1 2

--- R/visualization.R
+++ R/visualization.R
@@ -1488,10 +1488,12 @@ FeatureScatter <- function(
     cells = cells,
     slot = slot
   )
-  if (isFALSE(x = feature1 %in% colnames(x = data))) {
+  feature1 <- grep(pattern = paste0( feature1, "$"), colnames(data), value = T)
+  feature2 <- grep(pattern = paste0( feature2, "$"), colnames(data), value = T)
+  if (length(feature1) == 0) {
     stop("Feature 1 (", feature1, ") not found.", call. = FALSE)
   }
-  if (isFALSE(x = feature2 %in% colnames(x = data))) {
+  if (length(feature2) == 0) {
     stop("Feature 2 (", feature2, ") not found.", call. = FALSE)
   }
   data <- as.data.frame(x = data)
