ab8371a37d1bff18cbcc19311c1d715b05b8e05a
yuhanH <yh1970@nyu.edu>
2020-07-06 20:15:00 -0400

    update

--- R/visualization.R
+++ R/visualization.R
@@ -1488,12 +1488,12 @@ FeatureScatter <- function(
     cells = cells,
     slot = slot
   )
-  feature1 <- grep( pattern = paste0( feature1, "$"), colnames(x = data), value = TRUE)
-  feature2 <- grep( pattern = paste0( feature2, "$"), colnames(x = data), value = TRUE)
-  if (length(x = feature1) == 0) {
+  feature1 <- c(grep( pattern = paste0( "_", feature1, "$"), colnames(x = data), value = TRUE), feature1)[1]
+  feature2 <- c(grep( pattern = paste0( "_", feature2, "$"), colnames(x = data), value = TRUE), feature2)[1]
+  if (isFALSE(x = feature1 %in% colnames(x = data))) {
     stop("Feature 1 (", feature1, ") not found.", call. = FALSE)
   }
-  if (length(x = feature2) == 0) {
+  if (isFALSE(x = feature2 %in% colnames(x = data))) {
     stop("Feature 2 (", feature2, ") not found.", call. = FALSE)
   }
   data <- as.data.frame(x = data)
