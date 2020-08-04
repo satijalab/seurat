8d6258b038e223e10fe2aa1077f40a045fe1afa7
Charlotte Darby <cdarby@nygenome.org>
2020-08-04 10:32:06 -0400

    document and export NN functions

--- R/clustering.R
+++ R/clustering.R
@@ -6,6 +6,98 @@ NULL
 # Functions
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+#' Run Annoy approximate nearest neighbors algorithm
+#' 
+#' @details
+#' if `return.annoy_index = TRUE`, the returned object from AnnoySearch also contains
+#' a `metric` field (corresponding to the `metric` argument to this function), a `ndim` 
+#' field (the number of dimensions), and an `annoy_index` field which 
+#' is the index object.
+#
+#' @param data Data to build the index with
+#' @param query A set of data to be queried against data
+#' @param metric Distance metric; can be one of "euclidean", "cosine", "manhattan",
+#' "hamming"
+#' @param n.trees More trees gives higher precision when querying
+#' @param k Number of neighbors
+#' @param search.k During the query it will inspect up to search_k nodes which
+#' gives you a run-time tradeoff between better accuracy and speed.
+#' @param include.distance Include the corresponding distances
+#' @param nn.idx the precomputed AnnoyBuildIndex index
+#' @param return.annoy_index Return the index built by AnnoyBuildIndex?
+#'
+#' @export
+#'   
+AnnoyNN <- function(data, 
+                    query = data,
+                    metric = "euclidean", 
+                    n.trees = 50, 
+                    k,
+                    search.k = -1,
+                    include.distance = TRUE,
+                    nn.idx = NULL,
+                    return.annoy_index = FALSE) {
+  if (is.null(nn.idx)) {
+    idx <- AnnoyBuildIndex(
+      data = data,
+      metric = metric,
+      n.trees = n.trees)
+  } else {
+    idx <- nn.idx
+  }
+  nn <- AnnoySearch(
+    index = idx,
+    query = query,
+    k = k,
+    search.k = search.k,
+    include.distance = include.distance)
+  if (return.annoy_index) {
+    nn$metric <- metric
+    nn$ndim <- ncol(query)
+    nn$annoy_index <- idx
+  }
+  return(nn)
+}
+
+#' Load the Annoy neighbors and C++ index from RDS
+#' 
+#' @param file the name of the file where the nn object is saved by SaveAnnoyNN
+#' 
+#' @export
+#' 
+ReadAnnoyNN <- function(file){
+  file <- path.expand(file)
+  nn <- readRDS(file)
+  if (!is.null( nn$annoy_index.dir)) {
+    annoy_index <- uwot:::create_ann(name = nn$metric, ndim =  nn$ndim)
+    annoy_index$load( nn$annoy_index.dir)
+    nn$annoy_index <- annoy_index
+  }
+  return(nn)
+}
+
+
+
+#' Save the Annoy neighbors and C++ index to RDS
+#' 
+#' @param nn the output from AnnoyNN with return.annoy_index = T
+#' @param file the name of the file where the nn object is saved
+#' @param annoy_index_name suffix name for C++ index, which is saved 
+#' in a separate file
+#' 
+SaveAnnoyNN <- function( nn,
+                         file,
+                         annoy_index_name = "annoy_index"
+){
+  file <- path.expand(file)
+  if (!is.null(nn$annoy_index)) {
+    file.annoy_index <- paste(file, annoy_index_name, sep = ".")
+    nn$annoy_index$save(file.annoy_index)
+    nn$annoy_index.dir <- file.annoy_index
+  }
+  saveRDS(object = nn, file = file)
+}
+
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Methods for Seurat-defined generics
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -490,56 +582,13 @@ FindNeighbors.Seurat <- function(
 # Internal
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-# Run annoy
-#
-#' @param data Data to build the index with
-#' @param query A set of data to be queried against data
-#' @param metric Distance metric; can be one of "euclidean", "cosine", "manhattan",
-# "hamming"
-#' @param n.trees More trees gives higher precision when querying
-#' @param k Number of neighbors
-#' @param search.k During the query it will inspect up to search_k nodes which
-#' gives you a run-time tradeoff between better accuracy and speed.
-#' @param include.distance Include the corresponding distances
-#' @param nn.idx the precomputed AnnoyBuildIndex index
-#' @param return.annoy_index Determine if return index built by AnnoyBuildIndex
-#' 
-AnnoyNN <- function(data, 
-                    query = data,
-                    metric = "euclidean", 
-                    n.trees = 50, k,
-                    search.k = -1,
-                    include.distance = TRUE,
-                    nn.idx = NULL,
-                    return.annoy_index = FALSE) {
-  if (is.null(nn.idx)) {
-    idx <- AnnoyBuildIndex(
-      data = data,
-      metric = metric,
-      n.trees = n.trees)
-  } else {
-    idx <- nn.idx
-  }
-  nn <- AnnoySearch(
-    index = idx,
-    query = query,
-    k = k,
-    search.k = search.k,
-    include.distance = include.distance)
-  if (return.annoy_index) {
-    nn$metric <- metric
-    nn$ndim <- ncol(query)
-    nn$annoy_index <- idx
-  }
-  return(nn)
-}
-
-# Build the annoy index
+# Build the index for the Annoy approximate nearest neighbor algorithm
 #
 # @param data Data to build the index with
 # @param metric Distance metric; can be one of "euclidean", "cosine", "manhattan",
 # "hamming"
 # @param n.trees More trees gives higher precision when querying
+# 
 #' @importFrom RcppAnnoy AnnoyEuclidean AnnoyAngular AnnoyManhattan AnnoyHamming
 #
 AnnoyBuildIndex <- function(data, metric = "euclidean", n.trees = 50) {
@@ -559,14 +608,18 @@ AnnoyBuildIndex <- function(data, metric = "euclidean", n.trees = 50) {
   return(a)
 }
 
-# Search the annoy index
+# Search an Annoy approximate nearest neighbor index
 #
-# @param Annoy index, build with AnnoyBuildIndex
+# @param Annoy index, built with AnnoyBuildIndex
 # @param query A set of data to be queried against the index
 # @param k Number of neighbors
 # @param search.k During the query it will inspect up to search_k nodes which
 # gives you a run-time tradeoff between better accuracy and speed.
-# @ param include.distance Include the corresponding distances
+# @param include.distance Include the corresponding distances in the result
+#
+# @return A list with 'nn.idx' (for each element in 'query', the index of the 
+# nearest k elements in the index) and 'nn.dists' (the distances of the nearest
+# k elements)
 #
 AnnoySearch <- function(index, query, k, search.k = -1, include.distance = TRUE) {
   n <- nrow(x = query)
@@ -588,38 +641,6 @@ AnnoySearch <- function(index, query, k, search.k = -1, include.distance = TRUE)
     }
   }
   return(list(nn.idx = idx, nn.dists = dist))
-}
-
-# Save the annoy neighbors and C++ index to RDS
-#' @param nn the output from AnnoyNN with return.annoy_index = T
-#' @param file the name of the file where the nn object is saved
-#' @param annoy_index_name suffix name for C++ index
-#' 
-saveAnnoyNN <- function( nn,
-                         file,
-                         annoy_index_name = "annoy_index"
-){
-  file <- path.expand(file)
-  if (!is.null(nn$annoy_index)) {
-    file.annoy_index <- paste(file, annoy_index_name, sep = ".")
-    nn$annoy_index$save(file.annoy_index)
-    nn$annoy_index.dir <- file.annoy_index
-  }
-  saveRDS(object = nn, file = file)
-}
-
-# load the annoy neighbors and C++ index to RDS
-#' @param file the name of the file where the nn object is saved by saveAnnoyNN
-#' 
-readAnnoyNN <- function(file){
-  file <- path.expand(file)
-  nn <- readRDS(file)
-  if (!is.null( nn$annoy_index.dir)) {
-    annoy_index <- uwot:::create_ann(name = nn$metric, ndim =  nn$ndim)
-    annoy_index$load( nn$annoy_index.dir)
-    nn$annoy_index <- annoy_index
-  }
-  return(nn)
 }
 
 # Group single cells that make up their own cluster in with the cluster they are
