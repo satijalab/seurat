8bc90d8b02e235db0d74865cf40ff8d89883141a
Andrew <andrew.butler33@gmail.com>
2020-09-21 22:32:39 -0400

    refactor of TransferData

--- R/utilities.R
+++ R/utilities.R
@@ -1961,6 +1961,22 @@ Melt <- function(x) {
   ))
 }
 
+# Modify parameters in calling environment
+#
+# Used exclusively for helper parameter validation functions
+#
+# @param param name of parameter to change
+# @param value new value for parameter
+#
+ModifyParam <- function(param, value) {
+  # modify in original function environment
+  env1 <- sys.frame(which = length(x = sys.frames()) - 2)
+  env1[[param]] <- value
+  # also modify in validation function environment
+  env2 <- sys.frame(which = length(x = sys.frames()) - 1)
+  env2[[param]] <- value
+}
+
 # Give hints for old paramters and their newer counterparts
 #
 # This is a non-exhaustive list. If your function isn't working properly based
