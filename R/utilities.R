f5d41fca201c28f16458bd0521e3e5cf6932bc2c
Jaison Jain <jainj@satijalab04.nygenome.org>
2020-09-24 19:42:39 -0400

    style

--- R/utilities.R
+++ R/utilities.R
@@ -330,7 +330,7 @@ AverageExpression <- function(
     if (any(data.use == Inf)) {
       warning("Exponentiation yielded infinite values. `data` may not be log-normed.")
     }
-    data.return[[i]] <- as.matrix(data.use %*% as.sparse(category.matrix))
+    data.return[[i]] <- as.matrix(x = (data.use %*% as.sparse(x = category.matrix)))
     names(x = data.return)[i] <- assays[[i]]
   }
   if (return.seurat) {
