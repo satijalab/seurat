f7c6d1f35550d15590dc2cc66f4fadacd3a586e5
Paul Hoffman <mojaveazure@users.noreply.github.com>
2020-12-08 20:22:49 -0800

    Only show package startup message in interactive R sessions Add new option to prevent package startup messages

--- R/zzz.R
+++ R/zzz.R
@@ -25,6 +25,8 @@
 #'   Moran's I function available via the Rfast2 package}}
 #'   \item{\code{Seurat.warn.vlnplot.split}}{Show message about changes to
 #'   default behavior of split/multi violin plots}
+#'   \item{\code{Seurat.quietstart}}{Show package startup messages in
+#'   interactive sessions}
 #' }
 #'
 #' @docType package
@@ -39,15 +41,18 @@ seurat_default_options <- list(
   Seurat.checkdots = "warn",
   Seurat.limma.wilcox.msg = TRUE,
   Seurat.Rfast2.msg = TRUE,
-  Seurat.warn.vlnplot.split = TRUE
+  Seurat.warn.vlnplot.split = TRUE,
+  Seurat.quietstart = FALSE
 )
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste(
-    "\nSeurat v4 will be going to CRAN imminently;\n",
-    "for more details, please visit",
-    "https://satijalab.org/seurat/v4_changes\n"
-  ))
+  if (interactive() && !isTRUE(x = getOption(x = "Seurat.quietstart", default = FALSE))) {
+    packageStartupMessage(paste(
+      "\nSeurat v4 will be going to CRAN imminently;\n",
+      "for more details, please visit",
+      "https://satijalab.org/seurat/v4_changes\n"
+    ))
+  }
 }
 
 .onLoad <- function(libname, pkgname) {
