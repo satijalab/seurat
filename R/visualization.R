cdada851015ce30a858aec2082b7a6dbaf9aca87
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2021-09-13 14:38:34 -0400

    add violin raster

--- R/visualization.R
+++ R/visualization.R
@@ -554,6 +554,8 @@ RidgePlot <- function(
 #' single violin shapes.
 #' @param adjust Adjust parameter for geom_violin
 #' @param flip flip plot orientation (identities on x-axis)
+#' @param raster Convert points to raster format, default is \code{NULL} which
+#' automatically rasterizes if plotting more than 100,000 cells
 #'
 #' @return A \code{\link[patchwork]{patchwork}ed} ggplot object if
 #' \code{combine = TRUE}; otherwise, a list of ggplot objects
@@ -588,7 +590,8 @@ VlnPlot <- function(
   stack = FALSE,
   combine = TRUE,
   fill.by = 'feature',
-  flip = FALSE
+  flip = FALSE,
+  raster = NULL
 ) {
   if (
     !is.null(x = split.by) &
@@ -623,7 +626,8 @@ VlnPlot <- function(
     stack = stack,
     combine = combine,
     fill.by = fill.by,
-    flip = flip
+    flip = flip,
+    raster = raster
   ))
 }
 
@@ -5543,6 +5547,8 @@ DefaultDimReduc <- function(object, assay = NULL) {
 # ggplot object. If \code{FALSE}, return a list of ggplot objects
 # @param fill.by Color violins/ridges based on either 'feature' or 'ident'
 # @param flip flip plot orientation (identities on x-axis)
+# @param raster Convert points to raster format, default is \code{NULL} which
+# automatically rasterizes if plotting more than 100,000 cells
 #
 # @return A \code{\link[patchwork]{patchwork}ed} ggplot object if
 # \code{combine = TRUE}; otherwise, a list of ggplot objects
@@ -5571,7 +5577,8 @@ ExIPlot <- function(
   stack = FALSE,
   combine = TRUE,
   fill.by = NULL,
-  flip = FALSE
+  flip = FALSE,
+  raster = NULL
 ) {
   assay <- assay %||% DefaultAssay(object = object)
   DefaultAssay(object = object) <- assay
@@ -5672,7 +5679,8 @@ ExIPlot <- function(
         adjust = adjust,
         cols = cols,
         pt.size = pt.size,
-        log = log
+        log = log,
+        raster = raster
       ))
     }
   )
@@ -7183,6 +7191,8 @@ SingleDimPlot <- function(
 # @param cols Colors to use for plotting
 # @param log plot Y axis on log scale
 # @param seed.use Random seed to use. If NULL, don't set a seed
+# @param raster Convert points to raster format, default is \code{NULL} which
+# automatically rasterizes if plotting more than 100,000 cells
 #
 # @return A ggplot-based Expression-by-Identity plot
 #
@@ -7191,6 +7201,7 @@ SingleDimPlot <- function(
 #' @importFrom utils globalVariables
 #' @importFrom ggridges geom_density_ridges theme_ridges
 #' @importFrom ggplot2 ggplot aes_string theme labs geom_violin geom_jitter ylim position_jitterdodge
+#' @importFrom ggrastr rasterize
 #' scale_fill_manual scale_y_log10 scale_x_log10 scale_y_discrete scale_x_continuous waiver
 #' @importFrom cowplot theme_cowplot
 #'
@@ -7205,7 +7216,8 @@ SingleExIPlot <- function(
   pt.size = 0,
   cols = NULL,
   seed.use = 42,
-  log = FALSE
+  log = FALSE,
+  raster = NULL
 ) {
   if (!is.null(x = seed.use)) {
     set.seed(seed = seed.use)
@@ -7266,13 +7278,25 @@ SingleExIPlot <- function(
         theme(axis.text.x = element_text(angle = 45, hjust = 1))
       )
       if (is.null(x = split)) {
-        jitter <- geom_jitter(height = 0, size = pt.size, show.legend = FALSE)
+        if (isTRUE(x = raster)) {
+          jitter <- rasterize(geom_jitter(height = 0, size = pt.size, show.legend = FALSE))
+        } else {
+          jitter <- geom_jitter(height = 0, size = pt.size, show.legend = FALSE)
+        }
       } else {
-        jitter <- geom_jitter(
-          position = position_jitterdodge(jitter.width = 0.4, dodge.width = 0.9),
-          size = pt.size,
-          show.legend = FALSE
-        )
+        if (isTRUE(x = raster)) {
+          jitter <- rasterize(geom_jitter(
+            position = position_jitterdodge(jitter.width = 0.4, dodge.width = 0.9),
+            size = pt.size,
+            show.legend = FALSE
+          ))
+        } else {
+          jitter <- geom_jitter(
+            position = position_jitterdodge(jitter.width = 0.4, dodge.width = 0.9),
+            size = pt.size,
+            show.legend = FALSE
+          )
+        }
       }
       log.scale <- scale_y_log10()
       axis.scale <- ylim
