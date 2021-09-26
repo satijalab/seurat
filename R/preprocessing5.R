7add16e6b3c8f267d7d1806de7554a9bf4452b78
Paul Hoffman <mojaveazure@users.noreply.github.com>
2021-09-26 17:40:48 -0400

    Add new default NormalizeData for v5 Built to be matrix-backend agnostic Built to be directionality-agnostic Include optimized method for sparse matrices

--- R/preprocessing5.R
+++ R/preprocessing5.R
@@ -10,7 +10,7 @@ NULL
 
 #' @export
 #'
-LogNormalize5 <- function(data, scale.factor = 1e4, verbose = TRUE) {
+LogNormalize5 <- function(data, scale.factor = 1e4, margin = 2L, verbose = TRUE) {
   UseMethod(generic = 'LogNormalize5', object = data)
 }
 
@@ -22,28 +22,36 @@ LogNormalize5 <- function(data, scale.factor = 1e4, verbose = TRUE) {
 # Methods for Seurat-defined generics
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+#' @importFrom utils setTxtProgressBar txtProgressBar
+#'
 #' @method LogNormalize5 default
 #' @export
 #'
-LogNormalize5.default <- LogNormalize
-
-#' @importFrom utils txtProgressBar setTxtProgressBar
-#' @method LogNormalize5 spam
-#' @export
-#'
-LogNormalize5.spam <- function(data, scale.factor = 1e4, verbose = TRUE) {
-  PackageCheck('spam')
-  csums <- spam::colSums(data)
+LogNormalize5.default <- function(
+  data,
+  scale.factor = 1e4,
+  margin = 2L,
+  verbose = TRUE
+) {
+  margin <- SeuratObject:::.CheckFmargin(fmargin = margin)
+  ncells <- dim(x = data)[margin]
   if (isTRUE(x = verbose)) {
     pb <- txtProgressBar(file = stderr(), style = 3)
   }
-  for (i in seq_len(length.out = ncol(x = data))) {
-    idx <- which(x = slot(object = data, name = 'colindices') == i)
-    slot(object = data, name = 'entries')[idx] <- log1p(
-      x = slot(object = data, name = 'entries')[idx] / csums[i] * scale.factor
-    )
+  for (i in seq_len(length.out = ncells)) {
+    x <- if (margin == 1L) {
+      data[i, ]
+    } else {
+      data[, i]
+    }
+    xnorm <- log1p(x = x / sum(x) * scale.factor)
+    if (margin == 1L) {
+      data[i, ] <- xnorm
+    } else {
+      data[, i] <- xnorm
+    }
     if (isTRUE(x = verbose)) {
-      setTxtProgressBar(pb = pb, value = i / ncol(x = data))
+      setTxtProgressBar(pb = pb, value = i / ncells)
     }
   }
   if (isTRUE(x = verbose)) {
@@ -52,14 +60,58 @@ LogNormalize5.spam <- function(data, scale.factor = 1e4, verbose = TRUE) {
   return(data)
 }
 
-#' @importFrom SeuratObject .MARGIN DefaultLayer DefaultLayer<- LayerData LayerData<-
+#' @importFrom SeuratObject IsSparse .MARGIN
+#'
+#' @method NormalizeData default
+#' @export
+#'
+NormalizeData.default <- function(
+  object,
+  method = c('LogNormalize'),
+  scale.factor = 1e4,
+  cmargin = 2L,
+  margin = 1L,
+  block.size = NULL,
+  verbose = TRUE,
+  ...
+) {
+  method <- method[1L]
+  method <- match.arg(arg = method)
+  # TODO: enable parallelization via future
+  normalized <- switch(
+    EXPR = method,
+    'LogNormalize' = {
+      if (IsSparse(x = object) && .MARGIN(object = object) == cmargin) {
+        SparseNormalize(
+          data = object,
+          scale.factor = scale.factor,
+          verbose = verbose
+        )
+      } else {
+        LogNormalize5(
+          data = object,
+          scale.factor = scale.factor,
+          margin = cmargin,
+          verbose = verbose
+        )
+      }
+    }
+  )
+  return(normalized)
+}
+
+#' @importFrom SeuratObject .MARGIN Cells DefaultLayer DefaultLayer<- Features
+#' LayerData LayerData<-
 #'
 #' @method NormalizeData StdAssay
 #' @export
 #'
 NormalizeData.StdAssay <- function(
   object,
+  method = 'LogNormalize',
   scale.factor = 1e4,
+  margin = 1L,
+  block.size = NULL,
   layer = NULL,
   save = 'normalized',
   default = TRUE,
@@ -70,13 +122,22 @@ NormalizeData.StdAssay <- function(
   if (save == DefaultLayer(object = object)) {
     default <- FALSE
   }
-  data <- LayerData(object = object, layer = layer, fast = TRUE)
-  if (inherits(x = data, what = 'spam') && .MARGIN(object = object, type = 'cells') == 1) {
-    data <- SparseNormalize(data = data, scale.factor = scale.factor, verbose = verbose)
-  } else {
-    data <- LogNormalize5(data = data, scale.factor = scale.factor, verbose = verbose)
-  }
-  LayerData(object = object, layer = save) <- data
+  data <- NormalizeData(
+    object = LayerData(object = object, layer = layer, fast = TRUE),
+    method = method,
+    scale.factor = 1e4,
+    cmargin = .MARGIN(object = object, type = 'cells'),
+    margin = margin,
+    block.size = block.size,
+    verbose = verbose,
+    ...
+  )
+  LayerData(
+    object = object,
+    layer = save,
+    features = Features(x = object, layer = layer),
+    cells = Cells(x = object, layer = layer)
+  ) <- data
   if (isTRUE(x = default)) {
     DefaultLayer(object = object) <- save
   }
@@ -92,18 +153,23 @@ NormalizeData.StdAssay <- function(
 # Internal
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+#' @importFrom SeuratObject .SparseSlots
 #' @importFrom utils txtProgressBar setTxtProgressBar
 #'
 SparseNormalize <- function(data, scale.factor = 1e4, verbose = TRUE) {
-  p <- slot(object = data, name = 'rowpointers')
+  entryname <- .SparseSlots(x = data, type = 'entries')
+  p <- slot(object = data, name = .SparseSlots(x = data, type = 'pointers'))
+  if (p[1L] == 0) {
+    p <- p + 1L
+  }
+  np <- length(x = p) - 1L
   if (isTRUE(x = verbose)) {
     pb <- txtProgressBar(style = 3L, file = stderr())
   }
-  np <- length(x = p) - 1
   for (i in seq_len(length.out = np)) {
-    idx <- seq.int(from = p[i], to = p[i + 1] - 1)
-    xidx <- slot(object = data, name = 'entries')[idx]
-    slot(object = data, name = 'entries')[idx] <- log1p(
+    idx <- seq.int(from = p[i], to = p[i + 1] - 1L)
+    xidx <- slot(object = data, name = entryname)[idx]
+    slot(object = data, name = entryname)[idx] <- log1p(
       x = xidx / sum(xidx) * scale.factor
     )
     if (isTRUE(x = verbose)) {
@@ -115,3 +181,7 @@ SparseNormalize <- function(data, scale.factor = 1e4, verbose = TRUE) {
   }
   return(data)
 }
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# S4 Methods
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
