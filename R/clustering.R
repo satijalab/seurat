708973f132c0c76ed1c2a09cbce5cc367f1a4330
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2022-12-16 20:00:18 +0000

    Switch from leiden to leidenbase

--- R/clustering.R
+++ R/clustering.R
@@ -1706,17 +1706,17 @@ RunLeiden <- function(
     },
     stop("Method for Leiden must be either 'matrix' or igraph'")
   )
-  #run leiden from CRAN package (calls python with reticulate)
-  partition <- leiden(
+  # run clustering with leidenbase
+  partition <- leidenbase::leiden_find_partition(
     object = input,
     partition_type = partition.type,
     initial_membership = initial.membership,
-    weights = NULL,
+    edge_weights = NULL,
     node_sizes = node.sizes,
     resolution_parameter = resolution.parameter,
     seed = random.seed,
-    n_iterations = n.iter
-  )
+    num_iter = n.iter
+  )$membership
   return(partition)
 }
 
