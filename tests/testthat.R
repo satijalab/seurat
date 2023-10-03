f4c030051dfe165b69bb75f4262ca1fbf8066562
Gesmira <gesmiramolla@gmail.com>
2023-10-03 11:42:20 -0400

    fixing argument package msising issue

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,9 +4,9 @@ library(Seurat)
 # Run tests for 'v5'
 message('Run tests for v5 assay')
 options(Seurat.object.assay.version = 'v5')
-test_check()
+test_check("Seurat")
 
 # Run tests for 'v3'
 message('Run tests for v3 assay')
 options(Seurat.object.assay.version = 'v3')
-test_check()
+test_check("Seurat")
