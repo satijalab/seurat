2da392b33c7610c99830352c30afa97288145677
David Collins <dcollins@nygenome.org>
2024-03-18 15:27:19 -0400

    Fixup docs for LoadAkoya

--- R/convenience.R
+++ R/convenience.R
@@ -9,7 +9,6 @@ NULL
 
 #' @param fov Name to store FOV as
 #' @param assay Name to store expression matrix as
-#' @inheritDotParams ReadAkoya
 #'
 #' @return \code{LoadAkoya}: A \code{\link[SeuratObject]{Seurat}} object
 #'
