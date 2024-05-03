64fdfc5e348d1e60b9504f499040f944f2cb5e5b
David Collins <dcollins@nygenome.org>
2024-05-03 07:39:21 -0400

    Expand tests for BuildClusterTree

--- tests/testthat/test_tree.R
+++ tests/testthat/test_tree.R
@@ -1,5 +1,7 @@
 PATH_TO_DATA <- system.file("extdata", "pbmc_raw.txt", package = "Seurat")
 
+TOLERANCE = 1.0e-4
+
 setup_test_data <- function(cluster_resolution) {
     counts <- as.sparse(
         as.matrix(read.table(PATH_TO_DATA, sep = "\t", row.names = 1))
@@ -19,12 +21,12 @@ setup_test_data <- function(cluster_resolution) {
 
 context("BuildClusterTree")
 
-test_that("BuildClusterTree works as expected", {
+test_that("BuildClusterTree works as expected with `slot = 'counts'`", {
     # for testing purposes we'll over-cluster our dataset to introduce
     # some structure into the resulting phylogeny
-    test.case <- setup_test_data(cluster_resolution = 5)
+    test_case <- setup_test_data(cluster_resolution = 5)
 
-    result <- BuildClusterTree(test.case)
+    result <- BuildClusterTree(test_case, slot = "counts")
     tree <- Tool(result, slot = "BuildClusterTree")
 
     # TODO: this test doesn't really check that the phylogenic trees being
@@ -34,4 +36,30 @@ test_that("BuildClusterTree works as expected", {
     expect_equal(length(tree$tip.label), 6)
     # check that the tree contains the expected number of edges
     expect_equal(length(tree$edge), 20)
+    # spot check a few edge lengths
+    expect_equal(tree$edge.length[[1]], 1127.60199, tolerance = TOLERANCE)
+    expect_equal(tree$edge.length[[5]], 182.42944, tolerance = TOLERANCE)
+    expect_equal(tree$edge.length[[10]], 59.24525, tolerance = TOLERANCE)
+})
+
+
+test_that("BuildClusterTree works as expected with `slot = 'data'`", {
+    # for testing purposes we'll over-cluster our dataset to introduce
+    # some structure into the resulting phylogeny
+    test_case <- setup_test_data(cluster_resolution = 5)
+
+    result <- BuildClusterTree(test_case, slot = "data")
+    tree <- Tool(result, slot = "BuildClusterTree")
+
+    # TODO: this test doesn't really check that the phylogenic trees being
+    # generated are biologically valid but that would be more useful
+
+    # check that the tree contains the expected number of leaf nodes
+    expect_equal(length(tree$tip.label), 6)
+    # check that the tree contains the expected number of edges
+    expect_equal(length(tree$edge), 20)
+    # spot check a few edge lengths
+    expect_equal(tree$edge.length[[1]], 1206.4312, tolerance = TOLERANCE)
+    expect_equal(tree$edge.length[[5]], 657.0097, tolerance = TOLERANCE)
+    expect_equal(tree$edge.length[[10]], 982.9843, tolerance = TOLERANCE)
 })
