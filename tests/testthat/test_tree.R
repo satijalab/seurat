4569baa7562f05d39d7761260a3a47bc52477be8
David Collins <dcollins@nygenome.org>
2024-05-03 12:59:03 -0400

    Tidy test_tree.R

--- tests/testthat/test_tree.R
+++ tests/testthat/test_tree.R
@@ -22,16 +22,14 @@ setup_test_data <- function(cluster_resolution) {
 context("BuildClusterTree")
 
 test_that("BuildClusterTree works as expected with `slot = 'counts'`", {
+    # TODO: this test doesn't really check that the phylogenic trees being
+    # generated are biologically valid but that would be more useful
     # for testing purposes we'll over-cluster our dataset to introduce
     # some structure into the resulting phylogeny
     test_case <- setup_test_data(cluster_resolution = 5)
 
     result <- BuildClusterTree(test_case, slot = "counts")
     tree <- Tool(result, slot = "BuildClusterTree")
-
-    # TODO: this test doesn't really check that the phylogenic trees being
-    # generated are biologically valid but that would be more useful
-
     # check that the tree contains the expected number of leaf nodes
     expect_equal(length(tree$tip.label), 6)
     # check that the tree contains the expected number of edges
@@ -40,20 +38,9 @@ test_that("BuildClusterTree works as expected with `slot = 'counts'`", {
     expect_equal(tree$edge.length[[1]], 1127.60199, tolerance = TOLERANCE)
     expect_equal(tree$edge.length[[5]], 182.42944, tolerance = TOLERANCE)
     expect_equal(tree$edge.length[[10]], 59.24525, tolerance = TOLERANCE)
-})
-
-
-test_that("BuildClusterTree works as expected with `slot = 'data'`", {
-    # for testing purposes we'll over-cluster our dataset to introduce
-    # some structure into the resulting phylogeny
-    test_case <- setup_test_data(cluster_resolution = 5)
 
     result <- BuildClusterTree(test_case, slot = "data")
     tree <- Tool(result, slot = "BuildClusterTree")
-
-    # TODO: this test doesn't really check that the phylogenic trees being
-    # generated are biologically valid but that would be more useful
-
     # check that the tree contains the expected number of leaf nodes
     expect_equal(length(tree$tip.label), 6)
     # check that the tree contains the expected number of edges
