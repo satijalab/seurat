b0e81eed9a116544dc84afca960cebddfd9fde18
Gesmira <gesmiramolla@gmail.com>
2024-06-03 11:06:46 -0400

    update seed

--- tests/testthat/test_sketching.R
+++ tests/testthat/test_sketching.R
@@ -1,5 +1,4 @@
 # Tests for sketching related fxns
-set.seed(42)
 pbmc_small <- suppressWarnings(UpdateSeuratObject(pbmc_small))
 
 # Setup test object
@@ -12,7 +11,7 @@ context("SketchData")
 
 test_that("SketchData defaults work", {
   pbmc_sketched <- suppressWarnings(SketchData(pbmc_small, assay = "RNA", ncells = 50, method = "LeverageScore", 
-                              sketched.assay = "sketch"))
+                              sketched.assay = "sketch", set.seed = 42))
   expect_equal(dim(pbmc_sketched[["sketch"]]), c(230,50))
   expect_equal(as.numeric(pbmc_sketched$leverage.score[1]), 0.9036446, tolerance = 1e-6)
   expect_equal(colnames(pbmc_sketched[["sketch"]])[1], "ATGCCAGAACGACT")
@@ -28,26 +27,23 @@ pbmc_split <- suppressWarnings(split(pbmc_split, f = pbmc_split$groups))
 pbmc_split <- FindVariableFeatures(pbmc_split)
 
 test_that("SketchData with multiple layers works", { # (and one is less than the number of cells in that layer)
-  set.seed(42)
   pbmc_sketched <- suppressWarnings(SketchData(pbmc_split, assay = "RNA", ncells = 80, method = "LeverageScore", 
-                              sketched.assay = "sketch"))
+                              sketched.assay = "sketch", set.seed = 42))
   expect_equal(dim(pbmc_sketched[["sketch"]]$data.g1), c(230,80))
   expect_equal(dim(pbmc_sketched[["sketch"]]$data.g2), c(230,72))
-  expect_equal(as.numeric(pbmc_sketched$leverage.score[1]),  0.6290191, tolerance = 1e-6)
+  expect_equal(as.numeric(pbmc_sketched$leverage.score[1]), 0.7471112, tolerance = 1e-6)
   expect_equal(colnames(pbmc_sketched[["sketch"]])[1], "ATGCCAGAACGACT_1")
 })
 
 test_that("SketchData with a different number of cells per layer works", {
-  set.seed(42)
   pbmc_sketched <- suppressWarnings(SketchData(pbmc_split, assay = "RNA", ncells = c(50, 30), method = "LeverageScore", 
-                              sketched.assay = "sketch"))
+                              sketched.assay = "sketch", set.seed = 42))
   expect_equal(dim(pbmc_sketched[["sketch"]]$data.g1), c(230,30))
   expect_equal(dim(pbmc_sketched[["sketch"]]$data.g2), c(230,50))
-  expect_equal(as.numeric(pbmc_sketched$leverage.score[1]),  0.6290191, tolerance = 1e-6)
-  expect_equal(colnames(pbmc_sketched[["sketch"]])[1], "GAACCTGATGAACC_1")
-  set.seed(42)
+  expect_equal(as.numeric(pbmc_sketched$leverage.score[1]), 0.6220129, tolerance = 1e-6)
+  expect_equal(colnames(pbmc_sketched[["sketch"]])[1],  "ATGCCAGAACGACT_1")
   pbmc_sketched_2 <- suppressWarnings(SketchData(pbmc_split, assay = "RNA", ncells = c("data.g2" = 50, "data.g1" = 30), 
-                                method = "LeverageScore", sketched.assay = "sketch"))
+                                method = "LeverageScore", sketched.assay = "sketch", set.seed = 42))
   expect_equal(dim(pbmc_sketched_2[["sketch"]]$data.g1), dim(pbmc_sketched[["sketch"]]$data.g1))
   expect_equal(dim(pbmc_sketched_2[["sketch"]]$data.g2), dim(pbmc_sketched[["sketch"]]$data.g2))
   expect_equal(as.numeric(pbmc_sketched_2$leverage.score[1]), as.numeric(pbmc_sketched$leverage.score[1]), 
@@ -64,22 +60,22 @@ test_that("SketchData with specified features works", {
 })
 
 test_that("SketchData with specified features and multiple layers works", {
-  set.seed(42)
-  pbmc_sketched <- suppressWarnings(SketchData(pbmc_split, assay = "RNA", ncells = c(50, 30), method = "LeverageScore", 
-                              sketched.assay = "sketch", features = VariableFeatures(pbmc_small)[1:100]))
+  pbmc_sketched <- suppressWarnings(SketchData(pbmc_split, assay = "RNA", ncells = c(50, 30), 
+                              method = "LeverageScore", sketched.assay = "sketch",
+                              features = VariableFeatures(pbmc_small)[1:100], set.seed = 42))
   expect_equal(dim(pbmc_sketched[["sketch"]]$data.g1), c(230,30))
   expect_equal(dim(pbmc_sketched[["sketch"]]$data.g2), c(230,50))
-  expect_equal(as.numeric(pbmc_sketched$leverage.score[1]), 0.7197844, tolerance = 1e-6)
-  expect_equal(colnames(pbmc_sketched[["sketch"]])[1], "GAACCTGATGAACC_1")
+  expect_equal(as.numeric(pbmc_sketched$leverage.score[1]), 0.7324468, tolerance = 1e-6)
+  expect_equal(colnames(pbmc_sketched[["sketch"]])[1], "ATGCCAGAACGACT_1")
 })
 
 pbmc_new <- CreateSeuratObject(pbmc_small[["RNA"]]$counts)
 pbmc_new <- NormalizeData(pbmc_new, verbose = FALSE)
 VariableFeatures(pbmc_new) <- rownames(pbmc_new)[1:100]
 test_that("SketchData when setting your own variable features and specifying features works", {
-  set.seed(42)
-  pbmc_sketched <- suppressWarnings(suppressWarnings(SketchData(pbmc_new, assay = "RNA", ncells = 50, method = "LeverageScore", 
-                                                                sketched.assay = "sketch", features = VariableFeatures(pbmc_small)[1:100])))
+  pbmc_sketched <- suppressWarnings(SketchData(pbmc_new, assay = "RNA", ncells = 50, 
+                              method = "LeverageScore", sketched.assay = "sketch", 
+                              features = VariableFeatures(pbmc_small)[1:100], set.seed = 42))
   expect_equal(dim(pbmc_sketched[["sketch"]]), c(230,50))
   expect_equal(as.numeric(pbmc_sketched$leverage.score[1]), 0.7202896, tolerance = 1e-6)
   expect_equal(colnames(pbmc_sketched[["sketch"]])[1], "ATGCCAGAACGACT")
@@ -87,6 +83,7 @@ test_that("SketchData when setting your own variable features and specifying fea
 
 
 test_that("SketchData when setting your own variable features and not specifying features errors out", {
-  expect_error(suppressWarnings(SketchData(pbmc_new, assay = "RNA", ncells = 50, method = "LeverageScore", sketched.assay = "sketch")))
+  expect_error(suppressWarnings(SketchData(pbmc_new, assay = "RNA", ncells = 50, method = "LeverageScore", 
+                          sketched.assay = "sketch")))
 })
 
