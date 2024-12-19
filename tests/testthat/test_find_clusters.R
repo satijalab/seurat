375c1881a90b7769d59dd7d75609d793a0a2f466
David Collins <dcollins@nygenome.org>
2024-12-19 11:06:19 -0500

    Expand FindClusters smoke test with spot checks

--- tests/testthat/test_find_clusters.R
+++ tests/testthat/test_find_clusters.R
@@ -14,9 +14,15 @@ get_test_data <- function() {
   test_data <- FindVariableFeatures(test_data, verbose = FALSE)
   test_data <- ScaleData(test_data, verbose = FALSE)
   # Reduce number of PCs to avoid warning from `irlba` caused by the
-  # small size of the dataset being used.
-  test_data <- RunPCA(test_data, npcs = 20, verbose = FALSE)
-  test_data <- FindNeighbors(test_data, dims = 1:20, verbose = FALSE)
+  # small size of the dataset being used. Plus, we only want to build our
+  # SNN graph using the first 10 PCs to get "interesting" clustering results.
+  test_data <- RunPCA(test_data, npcs = 10, verbose = FALSE)
+  test_data <- FindNeighbors(
+    test_data,
+    k.param=10,
+    dims = 1:10,
+    verbose = FALSE
+  )
 
   return(test_data)
 }
@@ -28,8 +34,15 @@ context("FindClusters")
 test_that("Smoke test for `FindClusters`", {
   test_case <- get_test_data()
 
+  # Spot check cluster assignments with using defaults.
+  results <- FindClusters(test_case)$seurat_clusters
+  expect_equal(results[[1]], factor(3, levels=0:5))
+  expect_equal(results[[15]], factor(4, levels=0:5))
+  expect_equal(results[[24]], factor(0, levels=0:5))
+  expect_equal(results[[72]], factor(5, levels=0:5))
+  expect_equal(results[[length(results)]], factor(2, levels=0:5))
+
   # Check that every clustering algorithm can be run without errors.
-  expect_no_error(FindClusters(test_case))
   expect_no_error(FindClusters(test_case, algorithm = 1))
   expect_no_error(FindClusters(test_case, algorithm = 2))
   expect_no_error(FindClusters(test_case, algorithm = 3))
