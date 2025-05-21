satijalab/seurat

commit 6b1fc3e5e43517ce5983a56733414a581022534c

added conditional check for selecting a single sct model from integrated ref, updated validateparams to check for mixing sct and rna assays

R/integration.R
@@ -883,25 +883,27 @@ FindTransferAnchors <- function(
     new.names = paste0(Cells(x = reference), "_", "reference")
   )
   
-  # Select one SCT model if multiple detected in the reference
-  model_list <- reference[["SCT"]]@SCTModel.list
-  if (length(x = model_list) > 1) {
-    model_cell_counts <- sapply(
-      X = model_list,
-      FUN = function(model) {
-        length(x = Cells(x = model))
-      }
-    )
-    best_model_index <- which.max(model_cell_counts)
-    chosen_model <- model_list[[best_model_index]]
-    reference[["SCT"]] <- CreateSCTAssayObject(
-      data = GetAssayData(object = reference[["SCT"]], slot = "data"),
-      scale.data = GetAssayData(object = reference[["SCT"]], slot = "scale.data"),
-      SCTModel.list = chosen_model
-    )
-    message("Selected the SCT model fitted on the most cells.")
-  } else {
-    message("Only one SCT model detected; no need to select.")
+  # Select one SCT model based on max num of cells if multiple detected in the ref
+  if (normalization.method == "SCT"){
+    model_list <- reference[["SCT"]]@SCTModel.list
+    if (length(x = model_list) > 1) {
+      model_cell_counts <- sapply(
+        X = model_list,
+        FUN = function(model) {
+          length(x = Cells(x = model))
+        }
+      )
+      best_model_index <- which.max(model_cell_counts)
+      chosen_model <- model_list[[best_model_index]]
+      reference[["SCT"]] <- CreateSCTAssayObject(
+        data = GetAssayData(object = reference[["SCT"]], slot = "data"),
+        scale.data = GetAssayData(object = reference[["SCT"]], slot = "scale.data"),
+        SCTModel.list = chosen_model
+      )
+      message("Selected the SCT model fitted on the most cells.")
+    } else {
+      message("Only one SCT model detected; no need to select.")
+    }
   }
   
   # Perform PCA projection
@@ -6051,6 +6053,23 @@ ValidateParams_FindTransferAnchors <- function(
     recompute.residuals <- FALSE
     ModifyParam(param = "recompute.residuals", value = recompute.residuals)
   }
+  
+  # Added check for preventing SCT and RNA assay mixing 
+  if (normalization.method == "SCT") {
+    if (IsSCT(assay = reference[[reference.assay]]) && !IsSCT(assay = query[[query.assay]])) {
+      stop(
+        "Reference assay is SCT, but query assay is RNA. ",
+        "Mixing SCT and non-SCT in FindTransferAnchors is not supported."
+      )
+    }
+    if (!IsSCT(assay = reference[[reference.assay]]) && IsSCT(assay = query[[query.assay]])) {
+      stop(
+        "Reference assay is RNA, but query assay is SCT. ",
+        "Mixing SCT and non-SCT in FindTransferAnchors is not supported."
+      )
+    }
+  }
+  
   if (recompute.residuals) {
     # recompute.residuals only happens in ProjectCellEmbeddings, so k.filter set to NA.
     ModifyParam(param = "k.filter", value = NA)

commit 894a5895a29becca1614354f72525a7da963946d

fix bugs introduced to FindTransferAnchors from 6b1fc3e

R/integration.R
@@ -208,7 +208,7 @@ FindIntegrationAnchors <- function(
     scale <- FALSE
     if (is.numeric(x = anchor.features)) {
       stop("Please specify the anchor.features to be used. The expected ",
-      "workflow for integratinge assays produced by SCTransform is ",
+      "workflow for integrating assays produced by SCTransform is ",
       "SelectIntegrationFeatures -> PrepSCTIntegration -> ",
       "FindIntegrationAnchors.")
     }
@@ -885,7 +885,7 @@ FindTransferAnchors <- function(
   
   # Select one SCT model based on max num of cells if multiple detected in the ref
   if (normalization.method == "SCT"){
-    model_list <- reference[["SCT"]]@SCTModel.list
+    model_list <- reference[[reference.assay]]@SCTModel.list
     if (length(x = model_list) > 1) {
       model_cell_counts <- sapply(
         X = model_list,
@@ -896,8 +896,8 @@ FindTransferAnchors <- function(
       best_model_index <- which.max(model_cell_counts)
       chosen_model <- model_list[[best_model_index]]
       reference[["SCT"]] <- CreateSCTAssayObject(
-        data = GetAssayData(object = reference[["SCT"]], slot = "data"),
-        scale.data = GetAssayData(object = reference[["SCT"]], slot = "scale.data"),
+        data = LayerData(object = reference, assay = reference.assay, layer = "data"),
+        scale.data = LayerData(object = reference, assay = reference.assay, layer = "scale.data"),
         SCTModel.list = chosen_model
       )
       message("Selected the SCT model fitted on the most cells.")
@@ -6052,22 +6052,6 @@ ValidateParams_FindTransferAnchors <- function(
   if (normalization.method != "SCT") {
     recompute.residuals <- FALSE
     ModifyParam(param = "recompute.residuals", value = recompute.residuals)
-  }
-  
-  # Added check for preventing SCT and RNA assay mixing 
-  if (normalization.method == "SCT") {
-    if (IsSCT(assay = reference[[reference.assay]]) && !IsSCT(assay = query[[query.assay]])) {
-      stop(
-        "Reference assay is SCT, but query assay is RNA. ",
-        "Mixing SCT and non-SCT in FindTransferAnchors is not supported."
-      )
-    }
-    if (!IsSCT(assay = reference[[reference.assay]]) && IsSCT(assay = query[[query.assay]])) {
-      stop(
-        "Reference assay is RNA, but query assay is SCT. ",
-        "Mixing SCT and non-SCT in FindTransferAnchors is not supported."
-      )
-    }
   }
   
   if (recompute.residuals) {