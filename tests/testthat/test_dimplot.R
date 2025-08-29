d7e29dfb0a08fc18b86d0f41bafb90ca91a092ec
Nas5 <112696778+Nas5@users.noreply.github.com>
2025-08-29 13:52:22 +0100

    Fix DimPlot bug (#10061): prevent metadata columns from overriding reduction embeddings

--- tests/testthat/test_dimplot.R
+++ tests/testthat/test_dimplot.R
@@ -2,11 +2,11 @@
 test_that("DimPlot does not use meta.data columns for embeddings", {
   pbmc <- pbmc_small
   pbmc$PC_1 <- rep(1, ncol(pbmc)) # create bad metadata
-  
+
   p <- DimPlot(pbmc, reduction = "pca")
-  
+
   # show data used for ggplot
-  df <- layer_data(p)
-  
+  df <- ggplot2::layer_data(p)
+
   expect_true(var(df$x) > 0) # it shouldn't be constant (using real PCA values)
 })
