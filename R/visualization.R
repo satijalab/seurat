4a8ef098b534f5574968d8448e6e7d6d6aa501a0
YoukaiFromAccounting <brianzhang25@gmail.com>
2025-11-10 12:46:49 -0500

    Specify inherit.aes for geom.use

--- R/visualization.R
+++ R/visualization.R
@@ -6229,6 +6229,7 @@ LabelClusters <- function(
       data = labels.loc,
       mapping = aes(x = .data[[xynames['x']]], y = .data[[xynames['y']]], label = .data[[id]], fill = .data[["color"]]),
       show.legend = FALSE,
+      inherit.aes = FALSE,
       ...
     ) + scale_fill_identity()
   } else {
@@ -6237,6 +6238,7 @@ LabelClusters <- function(
       data = labels.loc,
       mapping = aes(x = .data[[xynames['x']]], y = .data[[xynames['y']]], label = .data[[id]]),
       show.legend = FALSE,
+      inherit.aes = FALSE,
       ...
     )
   }
